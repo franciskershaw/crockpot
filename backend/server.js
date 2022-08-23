149c18e9a6a64e330ec094582a172ea5a9fad7b6
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-08-23 21:18:40 +0100

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const path = require('path');
+const connectDB = require('./config/db');
+const colors = require('colors');
+// Grab port info from config
+const PORT = process.env.PORT || 5000;
+
+// Connect to Mongo database
+connectDB();
+
+// Initialize app
+const app = express();
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Routes
+// app.use('/api/users', require('./routes/userRoutes'));
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the crockpot API' });
+  });
+}
+
+// Listen for app
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
