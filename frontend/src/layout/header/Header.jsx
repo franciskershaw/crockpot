9b28107a5fba38a2d63172836a652ddce0fe0b9d
zoeAtomicSmash <zoe.thexton@atomicsmash.co.uk>
2022-09-30 15:33:29 +0100

    Header and nav updates

--- frontend/src/layout/header/Header.jsx
+++ frontend/src/layout/header/Header.jsx
@@ -1,7 +1,24 @@
+import { Link } from 'react-router-dom';
+import Icon from '../../components/global/icons/Icon'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faPlus, faMagnifyingGlass, faBook, faUtensils} from '@fortawesome/free-solid-svg-icons'
+
+
 const Header = () => {
   return (
-    <header className='header justify-center'>
-      HEADER
+    <header className='header'>
+      <div className="container flex justify-between items-center">
+        <div className="header__left">
+          <h1 className="header__title">Browse Recipes</h1>
+        </div>
+        <div className="header__right">
+          <Link to={'register'}>
+            <Icon>
+              <FontAwesomeIcon icon={faPlus}/>
+            </Icon>
+          </Link>
+        </div>
+      </div>
     </header>
   )
 }
