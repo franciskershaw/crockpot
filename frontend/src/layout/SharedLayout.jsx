f528baaaeef8b4cdfda257f390534f09550f1bab
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2022-10-01 11:10:56 +0100

    clean up SharedLayout

--- frontend/src/layout/SharedLayout.jsx
+++ frontend/src/layout/SharedLayout.jsx
@@ -1,6 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import Header from './header/Header';
 import Navbar from './navbar/Navbar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
