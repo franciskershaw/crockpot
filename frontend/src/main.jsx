34d98c3d35d528601425561218b534f85f378ea4
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2022-10-01 11:17:01 +0100

    initial set up of react query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.scss';
 import { ContextProvider } from './context/Context';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './reactQuery/queryClient.js';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <ContextProvider>
       <App />
     </ContextProvider>
-  </React.StrictMode>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
