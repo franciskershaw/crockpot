59c778d0e7970e8f29a51616c1ec507eceb79f58
Francis Kershaw <francis.kershaw@francisclark.co.uk>
2022-10-09 13:43:17 +0100

    start handling addRecipe form

--- frontend/src/components/forms/QuantityInput.jsx
+++ frontend/src/components/forms/QuantityInput.jsx
@@ -1,22 +1,28 @@
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faPlus, faMinus} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
-const QuantityInput = ({classes, step, label}) => {
-    const inputClasses = classes ? ` ${classes}` : '';
-    const inputStep = step ? step : '1';
+const QuantityInput = ({ classes, step, label, nameAndId }) => {
+  const inputClasses = classes ? ` ${classes}` : '';
+  const inputStep = step ? step : '1';
 
-    return (
-        <div className={`flex flex-col${inputClasses}`}>
-            {label ? (
-                <label htmlFor="" className='mb-1'>{label}</label>
-            ) : null}
-            <div className="form__input--quantity">
-                <div className="icon top-[-2px] left-[-2px]"><FontAwesomeIcon icon={faMinus}/></div>
-                <input type="number" min={0} step={inputStep} placeholder="0"/>
-                <div className="icon top-[-2px] right-[-2px]"><FontAwesomeIcon icon={faPlus}/></div>
-            </div>
+  return (
+    <div className={`flex flex-col${inputClasses}`}>
+      {label ? (
+        <label htmlFor={nameAndId} className="mb-1">
+          {label}
+        </label>
+      ) : null}
+      <div className="form__input--quantity">
+        <div className="icon top-[-2px] left-[-2px]">
+          <FontAwesomeIcon icon={faMinus} />
         </div>
-    )
-}
+        <input name={nameAndId} id={nameAndId} type="number" min={0} step={inputStep} placeholder="0" />
+        <div className="icon top-[-2px] right-[-2px]">
+          <FontAwesomeIcon icon={faPlus} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default QuantityInput
+export default QuantityInput;
--- frontend/src/pages/AddRecipePage/AddRecipePage.jsx
+++ frontend/src/pages/AddRecipePage/AddRecipePage.jsx
@@ -1,53 +1,78 @@
 import Header from '../../layout/header/Header';
-import Icon from '../../components/icons/Icon'
+import Icon from '../../components/icons/Icon';
 import PlusMinus from '../../components/buttons/PlusMinus';
 import QuantityInput from '../../components/forms/QuantityInput';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faPlus} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const AddRecipePage = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    image: {},
+    timeInMinutes: 30,
+    serves: 4,
+    categories: [],
+    ingredients: [],
+    instructions: [],
+    notes: [],
+  });
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
   return (
     <>
       <Header title="Add recipe">
-        <Icon classes={"ml-2"} type={"no-hover"}>
-          <FontAwesomeIcon icon={faPlus}/>
+        <Icon classes={'ml-2'} type={'no-hover'}>
+          <FontAwesomeIcon icon={faPlus} />
         </Icon>
       </Header>
-      <div className='container'>
-        <form className="form" id="addRecipe" action="">
+      <div className="container">
+        <form className="form" id="addRecipe">
           {/* Recipe name - 100 */}
           <div className="form__input">
-              <label htmlFor="recipeName">Recipe name</label>
-              <input
+            <label htmlFor="name">Recipe name</label>
+            <input
+              value={formData.name}
+              onChange={onChange}
               type="text"
-              id="recipeName"
-              name="recipeName"
+              id="name"
+              name="name"
               required
-              />
+            />
           </div>
           {/* Image - 100, file */}
           <div className="form__input">
-              <label htmlFor="image">Upload image</label>
-              <input
-              type="file"
-              id="image"
-              name="image"
-              required
-              />
+            <label htmlFor="image">Upload image</label>
+            <input type="file" id="image" name="image" required />
           </div>
           {/* Time, serves - 50 50, quantities  */}
-          <div className='flex justify-between'>
+          <div className="flex justify-between">
             <div className="form__input--50">
-              <QuantityInput label={"Time"} step={5} classes={"items-center"} />
+              <QuantityInput
+                nameAndId={'timeInMinutes'}
+                label={'Time'}
+                step={5}
+                classes={'items-center'}
+              />
             </div>
             <div className="form__input--50 items-center">
-              <QuantityInput label={"Serves"} step={1} classes={"items-center"} />
+              <QuantityInput
+                nameAndId={'serves'}
+                label={'Serves'}
+                step={1}
+                classes={'items-center'}
+              />
             </div>
           </div>
           {/* Categories - 100, select */}
           <div className="form__input">
             <label htmlFor="categories">Categories</label>
-            <select name='categories'>
+            <select name="categories">
               <option value="sensual">Sensual</option>
               <option value="erotic">Erotic</option>
               <option value="chickeeen">Chickeeen</option>
@@ -55,22 +80,31 @@ const AddRecipePage = () => {
           </div>
           <PlusMinus />
           {/* Ingredients - 50 25 25, select */}
-          <div className='flex justify-between'>
+          <div className="flex justify-between">
             <div className="form__input form__input--50">
               <label htmlFor="ingredient">Ingredients</label>
-              <select name='ingredient'>
+              <select name="ingredient">
                 <option value="apple">Apple</option>
                 <option value="banana">Banana</option>
                 <option value="chickeeen">Chickeeen</option>
               </select>
             </div>
             <div className="form__input form__input--25">
-              <label htmlFor="ingredientNum" className='invisible'>Quantities</label>
-              <input name="ingredientNum" type="number" min="0" placeholder="0"/>
+              <label htmlFor="ingredientNum" className="invisible">
+                Quantities
+              </label>
+              <input
+                name="ingredientNum"
+                type="number"
+                min="0"
+                placeholder="0"
+              />
             </div>
             <div className="form__input form__input--25">
-              <label htmlFor="ingredientUnit" className='invisible'>Units</label>
-              <select name='ingredientUnit'>
+              <label htmlFor="ingredientUnit" className="invisible">
+                Units
+              </label>
+              <select name="ingredientUnit">
                 <option value="">-</option>
                 <option value="g">g</option>
                 <option value="ml">ml</option>
@@ -80,29 +114,25 @@ const AddRecipePage = () => {
           <PlusMinus />
           {/* Instructions - 100 */}
           <div className="form__input">
-              <label htmlFor="instructions">Instructions</label>
-              <input
-              type="text"
-              id="instructions"
-              name="instructions"
-              required
-              />
+            <label htmlFor="instructions">Instructions</label>
+            <input type="text" id="instructions" name="instructions" required />
           </div>
           <PlusMinus />
           {/* Notes */}
           <div className="form__input">
-              <label htmlFor="notes">Notes</label>
-              <input
-              type="text"
-              id="notes"
-              name="notes"
-              required
-              />
+            <label htmlFor="notes">Notes</label>
+            <input type="text" id="notes" name="notes" required />
           </div>
           <PlusMinus />
         </form>
-        <div className='mt-5 text-center'>
-          <button class="btn btn--secondary" type="submit" form="addRecipe" value="Add Recipe">Add Recipe</button>
+        <div className="mt-5 text-center">
+          <button
+            className="btn btn--secondary"
+            type="submit"
+            form="addRecipe"
+            value="Add Recipe">
+            Add Recipe
+          </button>
         </div>
       </div>
     </>
