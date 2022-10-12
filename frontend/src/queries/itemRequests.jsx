ce2e4a33a0eb1efc57f633a04c7f7f96ca89c931
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2022-10-12 10:15:01 +0100

    write and test fetchItems query

--- frontend/src/queries/itemRequests.jsx
+++ frontend/src/queries/itemRequests.jsx
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
+export const fetchAllItems = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(`/api/items`, config);
+
+  return response.data;
+};
+
 export const fetchAllItemCategories = async (token) => {
   const config = {
     headers: {
