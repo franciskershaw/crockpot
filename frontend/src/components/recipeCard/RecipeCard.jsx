065998c4f030963570f39da176e6bf7f39ae588c
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2022-10-14 16:15:12 +0100

    render title and image on RecipeCard component

--- frontend/src/components/recipeCard/RecipeCard.jsx
+++ frontend/src/components/recipeCard/RecipeCard.jsx
@@ -1,29 +1,32 @@
 import { Link } from 'react-router-dom';
-import Icon from '../../components/icons/Icon'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faUtensils, faHeart} from '@fortawesome/free-solid-svg-icons'
+import Icon from '../../components/icons/Icon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils, faHeart } from '@fortawesome/free-solid-svg-icons';
 
-const RecipeCard = () => {
+const RecipeCard = ({ recipe }) => {
+  const { _id, name, image } = recipe;
   return (
     <div className="recipe-card">
-      <div className='recipe-card__image-container'>
-        <div className='recipe-card__icons flex justify-between p-1'>
-          <Icon type={"secondary"} outline>
-            <FontAwesomeIcon icon={faUtensils}/>
+      <div className="recipe-card__image-container">
+        <div className="recipe-card__icons flex justify-between p-1">
+          <Icon type={'secondary'} outline>
+            <FontAwesomeIcon icon={faUtensils} />
           </Icon>
-          <Icon type={"secondary"} outline>
-            <FontAwesomeIcon icon={faHeart}/>
+          <Icon type={'secondary'} outline>
+            <FontAwesomeIcon icon={faHeart} />
           </Icon>
         </div>
-        <div className='recipe-card__image'>
-          <img src="https://img.hellofresh.com/hellofresh_s3/image/pulled-chicken-burgers-3c693f20.jpg" alt=""></img>
+        <div className="recipe-card__image">
+          <img
+            src={image.url}
+            alt={image.name}></img>
         </div>
       </div>
-      <div className='recipe-card__title'>
-        <h4 className='text-white h5'>Pulled Chicken Burgers</h4>
+      <div className="recipe-card__title">
+        <h4 className="text-white h5">{name}</h4>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecipeCard
+export default RecipeCard;
