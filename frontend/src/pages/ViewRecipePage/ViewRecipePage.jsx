ea29acb9bc6b893da6cf0cb16a63819b7a1b779f
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2022-10-14 20:30:37 +0100

    render basic information on viewRecipe page

--- frontend/src/pages/ViewRecipePage/ViewRecipePage.jsx
+++ frontend/src/pages/ViewRecipePage/ViewRecipePage.jsx
@@ -1,6 +1,22 @@
+import { useRecipes } from '../../hooks/recipes/useRecipes'
+import { useParams } from 'react-router-dom'
+
 const ViewRecipePage = () => {
+	const { allRecipes } = useRecipes()
+	const params = useParams()
+	const recipe = allRecipes.find(recipe => recipe._id === params.id)
+	// All current recipe data lives in this recipe variable
+	console.log(recipe)
+		
 	return (
-		<div>View Recipe Page</div>
+		<div>
+			<h2>{recipe.name}</h2>
+			<ul>
+				{recipe.instructions.map((instruction, index) => (
+					<li key={`instruction_${index}`}>{instruction}</li>
+				))}
+			</ul>
+		</div>
 	)
 }
 
