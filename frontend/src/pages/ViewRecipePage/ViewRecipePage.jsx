7784ea1977b796e9d80ae916006205bf7fd69f62
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2022-10-20 11:45:23 +0100

    add temporary solution for incorrect recipe id in url

--- frontend/src/pages/ViewRecipePage/ViewRecipePage.jsx
+++ frontend/src/pages/ViewRecipePage/ViewRecipePage.jsx
@@ -1,19 +1,24 @@
 import { useCurrentRecipe } from '../../hooks/recipes/useCurrentRecipe';
+import { redirect } from 'react-router-dom';
 
 const ViewRecipePage = () => {
   const { recipe } = useCurrentRecipe();
-	console.log(recipe)
-
-  return (
-    <div>
-      <h2>{recipe.name}</h2>
-      <ul>
-        {recipe.instructions.map((instruction, index) => (
-          <li key={`instruction_${index}`}>{instruction}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  if (recipe) {
+    return (
+      <div>
+        <h2>{recipe.name}</h2>
+        <ul>
+          {recipe.instructions.map((instruction, index) => (
+            <li key={`instruction_${index}`}>{instruction}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <h2>No recipe found</h2>
+    )
+  }
 };
 
 export default ViewRecipePage;
