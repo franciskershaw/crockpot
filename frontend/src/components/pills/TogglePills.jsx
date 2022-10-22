7578f96970f3cabb179a8d4dfba6bfd524ca5d7b
zoeAtomicSmash <zoe.thexton@atomicsmash.co.uk>
2022-10-22 17:09:41 +0100

    Update TogglePills to show data dynamically

--- frontend/src/components/pills/TogglePills.jsx
+++ frontend/src/components/pills/TogglePills.jsx
@@ -1,40 +1,23 @@
 const TogglePills = ({name, type}) => {
+    const pills = [
+      'Orange',
+      'Banana',
+      'Apple',
+      'Purple',
+      'Big',
+      'Money',
+    ];
+
     const pillType = type ? ` toggle-pills--${type}` : ''; // Primary, secondary
   
     return (
       <ul className={`pills toggle-pills${pillType} text-center`}>
-        <li>
-          <input type="checkbox" id={`${name}_almonds`} value="almonds"/>
-          <label htmlFor={`${name}_almonds`}>Almonds</label>
-        </li>
-        <li>
-          <input type="checkbox" id="bananas" value="bananas"/>
-          <label htmlFor="bananas">bananas</label>
-        </li>
-        <li>
-          <input type="checkbox" id="cranberries" value="cranberries"/>
-          <label htmlFor="cranberries">cranberries</label>
-        </li>
-        <li>
-          <input type="checkbox" id="diamonds" value="diamonds"/>
-          <label htmlFor="diamonds">diamonds</label>
-        </li>
-        <li>
-          <input type="checkbox" id="electricity" value="electricity"/>
-          <label htmlFor="electricity">electricity</label>
-        </li>
-        <li>
-          <input type="checkbox" id="flames" value="flames"/>
-          <label htmlFor="flames">flames</label>
-        </li>
-        <li>
-          <input type="checkbox" id="grandma" value="grandma"/>
-          <label htmlFor="grandma">grandma</label>
-        </li>
-        <li>
-          <input type="checkbox" id="house" value="house"/>
-          <label htmlFor="house">house</label>
-        </li>
+        {pills.map((pill, index) => (
+          <li key={`pill_${index}`}>
+            <input type="checkbox" id={`${name}_${pill}`} value={pill}/>
+            <label htmlFor={`${name}_${pill}`}>{pill}</label>
+          </li>
+        ))}
       </ul>
     )
   }
