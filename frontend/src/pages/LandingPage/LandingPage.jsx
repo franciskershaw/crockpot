af40ecc1330fda8b524b495841176b638ac664d9
zoeAtomicSmash <zoe.thexton@atomicsmash.co.uk>
2022-10-27 08:35:51 +0100

    Initial work on accordions

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -13,6 +13,12 @@ import Icon from '../../components/icons/Icon'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faBook, faPlus, faMinus} from '@fortawesome/free-solid-svg-icons'
 import { useState, useEffect } from 'react';
+import {
+	Accordion,
+	AccordionItem,
+	AccordionButton,
+	AccordionPanel,
+} from "@reach/accordion";
 
 const LandingPage = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
@@ -273,7 +279,31 @@ const LandingPage = () => {
 				</div>
 				
 				<h3 className='text-blue-500 my-5'>Accordion</h3>
-
+				<div className='container'>
+					<Accordion collapsible multiple>
+						<AccordionItem>
+							<h3>
+							<AccordionButton>Step 1: Do a thing</AccordionButton>
+							</h3>
+							<AccordionPanel>
+							Here are some detailed instructions about doing a thing. I am very
+							complex and probably contain a lot of content, so a user can hide or
+							show me by clicking the button above.
+							</AccordionPanel>
+						</AccordionItem>
+						<AccordionItem>
+							<h3>
+							<AccordionButton>Step 2: Do another thing</AccordionButton>
+							</h3>
+							<AccordionPanel>
+							Here are some detailed instructions about doing yet another thing.
+							There are a lot of things someone might want to do, so I am only going
+							to talk about doing that other thing. I'll let my fellow accordion
+							items go into detail about even more things.
+							</AccordionPanel>
+						</AccordionItem>
+					</Accordion>
+				</div>
 			</div>
 
 			<div className='flex flex-col'>
