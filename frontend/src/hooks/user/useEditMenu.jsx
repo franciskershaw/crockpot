704a29fb174848f70198f5816a3056ca3253e068
Francis Kershaw <francis.kershaw@francisclark.co.uk>
2022-12-21 08:57:43 +0000

    Remove shopping list function and calls

--- frontend/src/hooks/user/useEditMenu.jsx
+++ frontend/src/hooks/user/useEditMenu.jsx
@@ -38,15 +38,6 @@ export function useEditMenu(recipeId) {
           menuRecipe._id === recipe._id &&
           menuRecipe.serves !== menuData.serves
         ) {
-          const shoppingList = generateShoppingList(
-            user.shoppingList,
-            'amendServes',
-            {
-              ingredients: recipe.ingredients,
-              serves: menuData.serves,
-              previousServes: menuRecipe.serves,
-            }
-          );
           editUser({
             recipeMenu: user.recipeMenu.map((menuRecipe) => {
               if (menuRecipe._id === recipe._id) {
@@ -54,97 +45,15 @@ export function useEditMenu(recipeId) {
               }
               return menuRecipe;
             }),
-            shoppingList,
           });
         }
       }
     }
   }, [menuData.serves]);
 
-  const generateShoppingList = (prevShoppingList, method, newRecipe) => {
-    let shoppingList = [];
-    // Needs to return an array of objects: IDs, quantities, units, obtained
-    if (method === 'add') {
-      for (let ingredient of newRecipe.ingredients) {
-        let existingItem = prevShoppingList.find(
-          (item) => item._id === ingredient._id
-        );
-        if (existingItem) {
-          existingItem.quantity =
-            existingItem.quantity + ingredient.quantity * newRecipe.serves;
-          shoppingList.push(existingItem);
-        } else if (!existingItem) {
-          shoppingList.push({
-            _id: ingredient._id,
-            quantity: ingredient.quantity * newRecipe.serves,
-            unit: ingredient.unit,
-            obtained: false,
-          });
-        }
-      }
-      for (let item of prevShoppingList) {
-        if (
-          !newRecipe.ingredients.find(
-            (ingredient) => ingredient._id === item._id
-          )
-        ) {
-          shoppingList.push(item);
-        }
-      }
-    } else if (method === 'remove') {
-      prevShoppingList.forEach((item) => {
-        let itemToBeAmended = newRecipe.ingredients.find(
-          (ingredient) => ingredient._id === item._id
-        );
-        if (itemToBeAmended) {
-          item.quantity =
-            item.quantity - itemToBeAmended.quantity * newRecipe.serves;
-          if (item.quantity !== 0) {
-            shoppingList.push(item);
-          }
-        } else {
-          shoppingList.push(item);
-        }
-      });
-    } else if (method === 'amendServes') {
-      if (newRecipe.previousServes > newRecipe.serves) {
-        shoppingList = prevShoppingList.map((item) => {
-          let itemToBeAmended = newRecipe.ingredients.find(
-            (ingredient) => ingredient._id === item._id
-          );
-          if (itemToBeAmended) {
-            return {
-              ...item,
-              quantity: item.quantity - itemToBeAmended.quantity,
-            };
-          }
-          return item;
-        });
-      } else if (newRecipe.serves > newRecipe.previousServes) {
-        shoppingList = prevShoppingList.map((item) => {
-          let itemToBeAmended = newRecipe.ingredients.find(
-            (ingredient) => ingredient._id === item._id
-          );
-          if (itemToBeAmended) {
-            return {
-              ...item,
-              quantity: item.quantity + itemToBeAmended.quantity,
-            };
-          }
-          return item;
-        });
-      }
-    }
-    return shoppingList;
-  };
-
   const onClickMenu = () => {
     // Adds recipe into menu
     if (!menuData.inMenu) {
-      const shoppingList = generateShoppingList(user.shoppingList, 'add', {
-        ingredients: recipe.ingredients,
-        serves: menuData.serves,
-      });
       editUser({
         recipeMenu: [
           ...user.recipeMenu,
@@ -153,7 +62,6 @@ export function useEditMenu(recipeId) {
             serves: menuData.serves,
           },
         ],
-        shoppingList,
       });
       setMenuData((prev) => ({
         ...prev,
@@ -162,15 +70,10 @@ export function useEditMenu(recipeId) {
 
       // Removes recipe from menu
     } else if (menuData.inMenu) {
-      const shoppingList = generateShoppingList(user.shoppingList, 'remove', {
-        ingredients: recipe.ingredients,
-        serves: menuData.serves,
-      });
       editUser({
         recipeMenu: user.recipeMenu.filter(
           (menuRecipe) => menuRecipe._id !== recipe._id
         ),
-        shoppingList,
       });
       setMenuData((prev) => ({
         ...prev,
