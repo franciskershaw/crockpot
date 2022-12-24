a4efba0fbe0cf7aff703819b1478ab41e38fa84e
Francis Kershaw <francis.kershaw@francisclark.co.uk>
2022-12-24 13:33:04 +0000

    Fix console warning about mongoose deprecation

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
 
 const connectDB = async () => {
-    try {
+    try {        
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDb Connected: ${conn.connection.host}`.cyan.underline)
     } catch (err) {
