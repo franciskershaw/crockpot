76d3920712e4258d8407d7e81851e99275cce822
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-01-03 16:39:49 +0000

    Fix: menu serves not updating in menu page

--- frontend/src/components/recipeCard/RecipeCardLong.jsx
+++ frontend/src/components/recipeCard/RecipeCardLong.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom';
-import Icon from '../icons/Icon'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faUtensils, faHeart, faTrash} from '@fortawesome/free-solid-svg-icons'
+import Icon from '../icons/Icon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faUtensils,
+  faHeart,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
 import QuantityInput from '../../components/forms/QuantityInput';
 import { useUser } from '../../hooks/auth/useUser';
 import { useEditMenu } from '../../hooks/user/useEditMenu';
@@ -9,28 +13,34 @@ import { useEditFavourites } from '../../hooks/user/useEditFavourites';
 
 const RecipeCardLong = ({ recipe, serves }) => {
   const { user } = useUser();
-  let recipeMenu
-  let recipeIds
+  let recipeMenu;
+  let recipeIds;
   if (user) {
-    recipeMenu = user.recipeMenu
+    recipeMenu = user.recipeMenu;
     recipeIds = recipeMenu.map((obj) => obj._id);
   }
   const { _id, name, image } = recipe;
-  const { onClickMenu } = useEditMenu(_id);
+  const { onClickMenu, menuData, setMenuData } = useEditMenu(_id);
   const onFavourite = useEditFavourites(recipe, user);
 
   return (
-    <div className='recipe-card-long'>
-      <div className='recipe-card-long__image-container'>
+    <div className="recipe-card-long">
+      <div className="recipe-card-long__image-container">
         <Link to={`/viewRecipe/${_id}`} className="recipe-card__image">
           <img src={image.url} alt={image.name}></img>
         </Link>
       </div>
-      <div className='recipe-card-long__content-container space-y-2'>
-        <h4 className='h4'>{name}</h4>
-        <div className='flex flex-col space-y-2'>
-          <QuantityInput value={serves} />
-          <div className='flex space-x-2'>
+      <div className="recipe-card-long__content-container space-y-2">
+        <h4 className="h4">{name}</h4>
+        <div className="flex flex-col space-y-2">
+          <QuantityInput
+            nameAndId={'serves'}
+            value={menuData.serves}
+            setValue={setMenuData}
+            step={1}
+            maxValue={20}
+          />
+          <div className="flex space-x-2">
             <Icon
               state={recipeIds.includes(recipe._id) ? 'active' : ''}
               type={'secondary'}
@@ -43,14 +53,14 @@ const RecipeCardLong = ({ recipe, serves }) => {
               outline>
               <FontAwesomeIcon onClick={onFavourite} icon={faHeart} />
             </Icon>
-            <Icon type={"secondary"} outline>
-              <FontAwesomeIcon icon={faTrash}/>
+            <Icon type={'secondary'} outline>
+              <FontAwesomeIcon icon={faTrash} />
             </Icon>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecipeCardLong
+export default RecipeCardLong;
