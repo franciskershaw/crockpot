c749be912e8597d223d926f35e2dac54473b3ac4
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-01-07 14:01:45 +0000

    Hide add recipe button for non admin

--- frontend/src/layout/navbar/Navbar.jsx
+++ frontend/src/layout/navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import { Link, useNavigate } from 'react-router-dom';
-import Icon from '../../components/icons/Icon'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faPlus, faMagnifyingGlass, faBook, faUtensils, faArrowRightFromBracket,
-  faArrowRightToBracket} from '@fortawesome/free-solid-svg-icons'
+import Icon from '../../components/icons/Icon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlus,
+  faMagnifyingGlass,
+  faBook,
+  faUtensils,
+  faArrowRightFromBracket,
+  faArrowRightToBracket,
+} from '@fortawesome/free-solid-svg-icons';
 import { useUser } from '../../hooks/auth/useUser';
 import { useAuth } from '../../hooks/auth/useAuth';
 
@@ -18,30 +24,34 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className='navbar'>
-        <div className='navbar__wrapper container'>
-          <div className='font-logo text-h1 text-black hidden md:block w-full text-center mb-2 lg:mb-0 lg:mr-auto lg:text-left lg:w-auto'>crockpot</div>
+      <nav className="navbar">
+        <div className="navbar__wrapper container">
+          <div className="font-logo text-h1 text-black hidden md:block w-full text-center mb-2 lg:mb-0 lg:mr-auto lg:text-left lg:w-auto">
+            crockpot
+          </div>
+          {user?.isAdmin && (
             <Link className="w-1/4 md:w-auto" to={'addrecipe'}>
-              <Icon text={"Add recipe"} outline>
-                <FontAwesomeIcon icon={faPlus}/>
+              <Icon text={'Add recipe'} outline>
+                <FontAwesomeIcon icon={faPlus} />
               </Icon>
             </Link>
-            <Link className="w-1/4 md:w-auto" to={'browse'}>
-              <Icon text={"Browse"} outline>
-                <FontAwesomeIcon icon={faMagnifyingGlass}/>
-              </Icon>
-            </Link>
-            <Link className="w-1/4 md:w-auto" to={'cookbook'}>
-              <Icon text={"Cookbook"} outline>
-                <FontAwesomeIcon icon={faBook}/>
-              </Icon>
-            </Link>
-            <Link className="w-1/4 md:w-auto" to={'menu'}>
-              <Icon text={"Menu"} outline>
-                <FontAwesomeIcon icon={faUtensils}/>
-              </Icon>
-            </Link>
-          <div className='hidden md:block'>
+          )}
+          <Link className="w-1/4 md:w-auto" to={'browse'}>
+            <Icon text={'Browse'} outline>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </Icon>
+          </Link>
+          <Link className="w-1/4 md:w-auto" to={'cookbook'}>
+            <Icon text={'Cookbook'} outline>
+              <FontAwesomeIcon icon={faBook} />
+            </Icon>
+          </Link>
+          <Link className="w-1/4 md:w-auto" to={'menu'}>
+            <Icon text={'Menu'} outline>
+              <FontAwesomeIcon icon={faUtensils} />
+            </Icon>
+          </Link>
+          <div className="hidden md:block">
             {user ? (
               <div onClick={onLogout} className="cursor-pointer">
                 <Icon text={'Log out'} outline>
@@ -59,7 +69,7 @@ const Navbar = () => {
         </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
