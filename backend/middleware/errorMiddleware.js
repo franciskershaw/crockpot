f2cc2edc393753762d81367ef522be318db76412
Francis Kershaw <francis.kershaw@francisclark.co.uk>
2023-01-13 12:57:03 +0000

    Edit backend middleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,16 +1,22 @@
 const errorHandler = (err, req, res, next) => {
-  console.log(err)
-  const statusCode = res.statusCode ? res.statusCode : 500;
+  if (process.env.NODE_ENV === 'development') {
+    console.log(err);
+  }
 
+  const statusCode = err.statusCode || 500;
+  let message = err.message;
+
+  if (statusCode === 500) {
+    message = 'An unexpected error occurred, please try again later.';
+  }
   res.status(statusCode);
 
-  // Only show stack if in developments
   res.json({
-      message: err.message,
-      stack: process.env.NODE_ENV === 'production' ? null : err.stack
-  })
-}
+    message: message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+};
 
 module.exports = {
   errorHandler,
-}
+};
