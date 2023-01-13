65699928557768b81e9635c9806f8c9ab51cad39
Francis Kershaw <francis.kershaw@francisclark.co.uk>
2023-01-13 12:57:23 +0000

    Edit login and registration routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
+const { BadRequestError, ConflictError, UnauthorizedError } = require('../errors/errors');
 
 const User = require('../models/User');
 const Recipe = require('../models/Recipe');
@@ -10,297 +11,294 @@ const Item = require('../models/Item');
 const { isLoggedIn, isRightUser } = require('../middleware/authMiddleware');
 
 // Register new user
-router.post('/', asyncHandler(async (req, res) => {
-		const { username, password } = req.body;
-
-		// Validation
-		if (!username || !password) {
-			res.status(400);
-			throw new Error('Please include all fields');
-		}
-
-		// Check user doesn't already exist
-		const userExists = await User.findOne({ username });
-
-		if (userExists) {
-			res.status(400);
-			throw new Error('User already exists');
-		}
-
-		// Hash password
-		const salt = await bcrypt.genSalt(10);
-		const hashedPassword = await bcrypt.hash(password, salt);
-
-		// Create user
-		const user = await User.create({
-			username,
-			password: hashedPassword,
-			isAdmin: false,
-			favouriteRecipes: [],
-			recipeMenu: [],
-			shoppingList: [],
-			regularItems: [],
-			extraItems: [],
-		});
-
-		if (user) {
-			res.status(201).json({
-				_id: user._id,
-				username: user.username,
-				isAdmin: user.isAdmin,
-				favouriteRecipes: user.favouriteRecipes,
-				recipeMenu: user.recipeMenu,
-				shoppingList: user.ShoppingList,
-				regularItems: user.regularItems,
-				extraItems: user.extraItems,
-				token: generateToken(user._id),
-			});
-		} else {
-			res.status(400);
-			throw new Error('Invalid user data');
-		}
-	})
+router.post('/', asyncHandler(async (req, res, next) => {
+    try {
+      const { username, password } = req.body;
+      // Validation
+      if (!username || !password) {
+        throw new BadRequestError('Please include all fields');
+      }
+      // Check user doesn't already exist
+      const userExists = await User.findOne({ username });
+      if (userExists) {
+        throw new ConflictError('User already exists');
+      }
+      // Hash password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      // Create user
+      const user = await User.create({
+        username,
+        password: hashedPassword,
+        isAdmin: false,
+        favouriteRecipes: [],
+        recipeMenu: [],
+        shoppingList: [],
+        regularItems: [],
+        extraItems: [],
+      });
+      if (user) {
+        res.status(201).json({
+          _id: user._id,
+          username: user.username,
+          isAdmin: user.isAdmin,
+          favouriteRecipes: user.favouriteRecipes,
+          recipeMenu: user.recipeMenu,
+          shoppingList: user.shoppingList,
+          regularItems: user.regularItems,
+          extraItems: user.extraItems,
+          token: generateToken(user._id),
+        });
+      } else {
+        throw new BadRequestError('Invalid user data', 400);
+      }
+    } catch (err) {
+      next(err);
+    }
+  })
 );
 
 // Login a user
-router.post('/login', asyncHandler(async (req, res) => {
-		try {
-			const { username, password } = req.body;
-
-			const user = await User.findOne({ username });
-
-			// Check credentials match
-			if (user && (await bcrypt.compare(password, user.password))) {
-				res.status(200).json({
-					_id: user._id,
-					username: user.username,
-					isAdmin: user.isAdmin,
-					favouriteRecipes: user.favouriteRecipes,
-					recipeMenu: user.recipeMenu,
-					shoppingList: user.shoppingList,
-					regularItems: user.regularItems,
-					extraItems: user.extraItems,
-					token: generateToken(user._id),
-				});
-			} else {
-				res.status(401).json({ message: 'Invalid credentials' });
-				// throw new Error('Invalid credentials')
-			}
-		} catch (err) {
-			console.log(err);
-			throw new Error(err);
-		}
-	})
-);
+router.post('/login', asyncHandler(async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      throw new UnauthorizedError('Username or password is incorrect');
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw new UnauthorizedError('Username or password is incorrect');
+    }
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      isAdmin: user.isAdmin,
+      favouriteRecipes: user.favouriteRecipes,
+      recipeMenu: user.recipeMenu,
+      shoppingList: user.shoppingList,
+      regularItems: user.regularItems,
+      extraItems: user.extraItems,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    next(err);
+  }
+}));
 
 // Get username by ID
 router.get('/:userId', isLoggedIn, asyncHandler(async (req, res) => {
-		try {
-			const user = await User.findById(req.params.userId);
-			res.status(200).json({
-				username: user.username,
-			});
-		} catch (err) {
-			throw new Error("Can't find user");
-		}
-	})
+    try {
+      const user = await User.findById(req.params.userId);
+      res.status(200).json({
+        username: user.username,
+      });
+    } catch (err) {
+      throw new Error("Can't find user");
+    }
+  })
 );
 
 // Get recipe menu from user
 router.get('/:userId/recipeMenu', isLoggedIn, isRightUser, asyncHandler(async (req, res) => {
-		try {
-			const { recipeMenu } = await User.findById(req.params.userId);
-			const recipes = await Recipe.find({ _id: { $in: recipeMenu } });
-			const menu = [];
+    try {
+      const { recipeMenu } = await User.findById(req.params.userId);
+      const recipes = await Recipe.find({ _id: { $in: recipeMenu } });
+      const menu = [];
 
-			for (const recipe of recipes) {
-				const { serves } = recipeMenu.find((item) =>
-					item._id.equals(recipe._id)
-				);
-				menu.push({ recipe, serves });
-			}
+      for (const recipe of recipes) {
+        const { serves } = recipeMenu.find((item) =>
+          item._id.equals(recipe._id)
+        );
+        menu.push({ recipe, serves });
+      }
 
-			res.status(200).json(menu);
-		} catch (err) {
-			res.status(400);
-			throw new Error(err);
-		}
-	})
+      res.status(200).json(menu);
+    } catch (err) {
+      res.status(400);
+      throw new Error(err);
+    }
+  })
 );
 
 // Get shopping list from user
 router.get('/:userId/shoppingList', isLoggedIn, isRightUser, asyncHandler(async (req, res) => {
-		try {
-			const { shoppingList, extraItems } = await User.findById(req.params.userId);
-			const shoppingListItems = await Item.find({ _id: { $in: shoppingList } });
+    try {
+      const { shoppingList, extraItems } = await User.findById(
+        req.params.userId
+      );
+      const shoppingListItems = await Item.find({ _id: { $in: shoppingList } });
 
-			let list = [];
+      let list = [];
 
-			for (const item of shoppingListItems) {
-				const { quantity, unit, obtained } = shoppingList.find((shoppingListItem) =>
-					item._id.equals(shoppingListItem._id)
-				);
-				list.push({ item, quantity, unit, obtained });
-			}
+      for (const item of shoppingListItems) {
+        const { quantity, unit, obtained } = shoppingList.find(
+          (shoppingListItem) => item._id.equals(shoppingListItem._id)
+        );
+        list.push({ item, quantity, unit, obtained });
+      }
 
-			for (const item of extraItems) {
-				const existing = list.find(obj => obj.item._id.equals(item._id) && obj.unit === item.unit)
-				if (existing) {
-					list = list.map(obj => {
-						if (obj.item._id.equals(item._id) && obj.unit === item.unit) {
-							return {...obj, quantity: obj.quantity + item.quantity}
-						}
-						return obj
-					})
-				} else {
-					const { quantity, unit, obtained} = item
-					const additional = await Item.findById(item._id)
-					list.push({ item: additional, quantity, unit, obtained })
-				}
-			}
-			
-			res.status(200).json(list);
-		} catch (err) {
-			res.status(400);
-			throw new Error(err);
-		}
-	})
+      for (const item of extraItems) {
+        const existing = list.find(
+          (obj) => obj.item._id.equals(item._id) && obj.unit === item.unit
+        );
+        if (existing) {
+          list = list.map((obj) => {
+            if (obj.item._id.equals(item._id) && obj.unit === item.unit) {
+              return { ...obj, quantity: obj.quantity + item.quantity };
+            }
+            return obj;
+          });
+        } else {
+          const { quantity, unit, obtained } = item;
+          const additional = await Item.findById(item._id);
+          list.push({ item: additional, quantity, unit, obtained });
+        }
+      }
+
+      res.status(200).json(list);
+    } catch (err) {
+      res.status(400);
+      throw new Error(err);
+    }
+  })
 );
 
 // Get favourites from user
 router.get('/:userId/favourites', isLoggedIn, isRightUser, asyncHandler(async (req, res) => {
-		try {
-			const { favouriteRecipes } = await User.findById(req.params.userId);
-			const favourites = await Recipe.find({ _id: { $in: favouriteRecipes } });
+    try {
+      const { favouriteRecipes } = await User.findById(req.params.userId);
+      const favourites = await Recipe.find({ _id: { $in: favouriteRecipes } });
 
-			res.status(200).json(favourites);
-		} catch (err) {
-			res.status(400);
-			throw new Error(err);
-		}
-	})
+      res.status(200).json(favourites);
+    } catch (err) {
+      res.status(400);
+      throw new Error(err);
+    }
+  })
 );
 
 // Edit user
 router.put('/:userId', isLoggedIn, isRightUser, asyncHandler(async (req, res) => {
-		try {
-			let userToUpdate = await User.findById(req.params.userId);
-			let shoppingList;
-			let extraItems;
+    try {
+      let userToUpdate = await User.findById(req.params.userId);
+      let shoppingList;
+      let extraItems;
 
-			if (req.body.recipeMenu) {
-				shoppingList = await generateShoppingList(req.body.recipeMenu);
-			} else {
-				shoppingList = userToUpdate.shoppingList;
-			}
+      if (req.body.recipeMenu) {
+        shoppingList = await generateShoppingList(req.body.recipeMenu);
+      } else {
+        shoppingList = userToUpdate.shoppingList;
+      }
 
-			if (req.body.extraItems) {
-				if (req.body.extraItems.length) {
-					extraItems = userToUpdate.extraItems
-					extraItems.push(req.body.extraItems[0])
-				} else if (!req.body.extraItems.length) {
-					// Pass in an empty array from the frontend if the user is to clear extraItems
-					extraItems = []
-				}
-			} else {
-				extraItems = userToUpdate.extraItems
-			}
+      if (req.body.extraItems) {
+        if (req.body.extraItems.length) {
+          extraItems = userToUpdate.extraItems;
+          extraItems.push(req.body.extraItems[0]);
+        } else if (!req.body.extraItems.length) {
+          // Pass in an empty array from the frontend if the user is to clear extraItems
+          extraItems = [];
+        }
+      } else {
+        extraItems = userToUpdate.extraItems;
+      }
 
-			let updates = { ...req.body, shoppingList, extraItems };
-			const updatedUser = await User.findByIdAndUpdate(
-				req.params.userId,
-				updates,
-				{ new: true }
-			);
-			if (req.body.recipeMenu) {
-				const newShoppingList = await generateShoppingList(
-					updatedUser.recipeMenu
-				);
-				updatedUser.shoppingList = newShoppingList;
-				updatedUser.save();
-			}
-			res.status(200).json(updatedUser);
-		} catch (err) {
-			res.status(400);
-			throw new Error(err);
-		}
-	})
+      let updates = { ...req.body, shoppingList, extraItems };
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        updates,
+        { new: true }
+      );
+      if (req.body.recipeMenu) {
+        const newShoppingList = await generateShoppingList(
+          updatedUser.recipeMenu
+        );
+        updatedUser.shoppingList = newShoppingList;
+        updatedUser.save();
+      }
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(400);
+      throw new Error(err);
+    }
+  })
 );
 
 router.put('/:userId/shoppingList', isLoggedIn, isRightUser, asyncHandler(async (req, res) => {
-	try {
-		let userToUpdate = await User.findById(req.params.userId)
-		let shoppingList = userToUpdate.shoppingList
-		let extraItems = userToUpdate.extraItems
+    try {
+      let userToUpdate = await User.findById(req.params.userId);
+      let shoppingList = userToUpdate.shoppingList;
+      let extraItems = userToUpdate.extraItems;
 
-		for (let item of shoppingList) {
-			if (item._id.equals(req.body.recipeId)) {
-				item.obtained = !item.obtained
-			}
-		}
+      for (let item of shoppingList) {
+        if (item._id.equals(req.body.recipeId)) {
+          item.obtained = !item.obtained;
+        }
+      }
 
-		for (let item of extraItems) {
-			if (item._id.equals(req.body.recipeId)) {
-				item.obtained = !item.obtained
-			}
-		}
-		
-		userToUpdate.shoppingList = shoppingList
-		userToUpdate.extraItems = extraItems
-		userToUpdate.save()
-		res.status(200).json(shoppingList)
-	} catch (err) {
-		res.status(400);
-		throw new Error(err);
-	}
-}))
+      for (let item of extraItems) {
+        if (item._id.equals(req.body.recipeId)) {
+          item.obtained = !item.obtained;
+        }
+      }
+
+      userToUpdate.shoppingList = shoppingList;
+      userToUpdate.extraItems = extraItems;
+      userToUpdate.save();
+      res.status(200).json(shoppingList);
+    } catch (err) {
+      res.status(400);
+      throw new Error(err);
+    }
+  })
+);
 
 const generateShoppingList = async (menu) => {
-	let shoppingList = [];
-	if (menu.length) {
-		for (let object of menu) {
-			const recipe = await Recipe.findById(object._id).select({
-				ingredients: 1,
-			});
-			const ingredients = recipe.toObject().ingredients;
-			let ingredientsFormated = ingredients.map((ingredient) => {
-				return {
-					...ingredient,
-					quantity: ingredient.quantity * object.serves,
-					obtained: false,
-				};
-			});
-			shoppingList = [
-				...shoppingList.filter(
-					(obj) =>
-						!ingredientsFormated.some(
-							(newObj) => newObj._id.equals(obj._id) && newObj.unit === obj.unit
-						)
-				),
-				...ingredientsFormated.map((obj) => {
-					const originalObj = shoppingList.find(
-						(originalObj) =>
-							originalObj._id.equals(obj._id) && originalObj.unit === obj.unit
-					);
-					if (originalObj) {
-						return {
-							...obj,
-							quantity: originalObj.quantity + obj.quantity,
-						};
-					}
-					return obj;
-				}),
-			];
-		}
-	}
-	return shoppingList;
+  let shoppingList = [];
+  if (menu.length) {
+    for (let object of menu) {
+      const recipe = await Recipe.findById(object._id).select({
+        ingredients: 1,
+      });
+      const ingredients = recipe.toObject().ingredients;
+      let ingredientsFormated = ingredients.map((ingredient) => {
+        return {
+          ...ingredient,
+          quantity: ingredient.quantity * object.serves,
+          obtained: false,
+        };
+      });
+      shoppingList = [
+        ...shoppingList.filter(
+          (obj) =>
+            !ingredientsFormated.some(
+              (newObj) => newObj._id.equals(obj._id) && newObj.unit === obj.unit
+            )
+        ),
+        ...ingredientsFormated.map((obj) => {
+          const originalObj = shoppingList.find(
+            (originalObj) =>
+              originalObj._id.equals(obj._id) && originalObj.unit === obj.unit
+          );
+          if (originalObj) {
+            return {
+              ...obj,
+              quantity: originalObj.quantity + obj.quantity,
+            };
+          }
+          return obj;
+        }),
+      ];
+    }
+  }
+  return shoppingList;
 };
 
 // Generate token
 const generateToken = (id) => {
-	return jwt.sign({ id }, process.env.JWT_SECRET, {
-		expiresIn: '30d',
-	});
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
 };
 
 module.exports = router;
