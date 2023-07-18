cb93788ed6bda6b762a32ca6c8217613482ea540
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-07-18 21:37:44 +0100

    render basic radix form

--- client/src/app/sandbox/auth/components/AuthForm.tsx
+++ client/src/app/sandbox/auth/components/AuthForm.tsx
@@ -1,17 +1,51 @@
+'use client';
+
+import * as Form from '@radix-ui/react-form';
+
 interface Props {
   type: 'register' | 'login';
 }
 
 const AuthForm = (props: Props) => {
-	const heading = props.type === 'register' ? 'Sign up for an account' : 'Login'
+  const heading =
+    props.type === 'register' ? 'Sign up for an account' : 'Login';
+  const buttonText = props.type === 'register' ? 'Sign up' : 'Login';
   return (
-    <form className='border border-black rounded-sm py-6 px-4'>
-			<h3 className='text-center'>{heading}</h3>
-			<div className='flex flex-col'>
-				<label htmlFor="">Username</label>
-				<input type="text" />
-			</div>
-    </form>
+    <Form.Root className="border border-black rounded-sm px-6 py-8">
+      <h2 className="text-center mb-4">{heading}</h2>
+      <Form.Field name="username">
+        <div className="flex flex-col mb-4">
+          <Form.Label className="text-sm">Username</Form.Label>
+          <Form.Control asChild>
+            <input className="h-10" type="text" required />
+          </Form.Control>
+        </div>
+      </Form.Field>
+      <Form.Field name="password">
+        <div className="flex flex-col mb-4">
+          <Form.Label className="text-sm">Password</Form.Label>
+          <Form.Control asChild>
+            <input className="h-10" type="password" required />
+          </Form.Control>
+        </div>
+      </Form.Field>
+      {props.type === 'register' && (
+        <Form.Field name="confirmPassword">
+          <div className="flex flex-col mb-4">
+            <Form.Label className="text-sm">Confirm password</Form.Label>
+            <Form.Control asChild>
+              <input className="h-10" type="password" required />
+            </Form.Control>
+          </div>
+        </Form.Field>
+      )}
+
+      <Form.Submit asChild>
+        <button className="w-full border border-black h-10 rounded-sm mt-4">
+          {buttonText}
+        </button>
+      </Form.Submit>
+    </Form.Root>
   );
 };
 export default AuthForm;
--- client/src/app/sandbox/auth/page.tsx
+++ client/src/app/sandbox/auth/page.tsx
@@ -1,10 +1,15 @@
-import AuthForm from './components/AuthForm'
+import AuthForm from './components/AuthForm';
 
 const AuthComponentsPage = () => {
-	return (
-		<div className='tw h-screen p-2'>
-			<AuthForm type='login' />
-		</div>
-	)
-}
-export default AuthComponentsPage
+  return (
+    <div className="tw p-4">
+      <div className="mb-4">
+        <AuthForm type="login" />
+      </div>
+      <div>
+        <AuthForm type="register" />
+      </div>
+    </div>
+  );
+};
+export default AuthComponentsPage;
