266410733b0f3fff7405b97b2de8fb7d36fb16ed
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-07-23 17:31:16 +0100

    Uninstall radix form for now

--- client/src/app/sandbox/auth/components/AuthForm.tsx
+++ client/src/app/sandbox/auth/components/AuthForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import * as Form from '@radix-ui/react-form';
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 interface Props {
   type: 'register' | 'login';
@@ -12,75 +12,71 @@ const AuthForm = (props: Props) => {
   const [password, setPassword] = useState<string>('');
   const [confirmPassword, setConfirmPassword] = useState<string>('');
 
-  useEffect(() => {
-    console.log(username);
-  }, [username]);
-
-  const heading =
-    props.type === 'register' ? 'Sign up for an account' : 'Login';
-  const buttonText = props.type === 'register' ? 'Sign up' : 'Login';
   return (
-    <Form.Root className="border border-black rounded-sm px-6 py-8">
-      <h2 className="text-center mb-4">{heading}</h2>
-      <Form.Field name="username">
-        <div className="flex flex-col mb-4">
-          <Form.Label className="text-sm">Username</Form.Label>
-          <Form.Control asChild>
-            <input
-              name="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="h-10"
-              type="text"
-              id="username"
-              autoComplete="off"
-            />
-          </Form.Control>
-        </div>
-      </Form.Field>
-      <Form.Field name="password">
-        <div className="flex flex-col mb-4">
-          <Form.Label className="text-sm">Password</Form.Label>
-          <Form.Control asChild>
-            <input
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="h-10"
-              type="password"
-              id="password"
-              autoComplete="off"
-              required
-            />
-          </Form.Control>
-        </div>
-      </Form.Field>
-      {props.type === 'register' && (
-        <Form.Field name="confirmPassword">
-          <div className="flex flex-col mb-4">
-            <Form.Label className="text-sm">Confirm password</Form.Label>
-            <Form.Control asChild>
-              <input
-                name="confirmPassword"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                className="h-10"
-                type="password"
-                id="confirmPassword"
-                autoComplete="off"
-                required
-              />
-            </Form.Control>
-          </div>
-        </Form.Field>
-      )}
+    <form className="border border-black rounded-sm px-6 py-8 max-w-2xl">
+      <h2 className="text-center mb-4">
+        {props.type === 'login' ? 'Login' : 'Sign up for an account'}
+      </h2>
+      <div className="flex flex-col mb-4">
+        <label htmlFor="username" className="text-sm">
+          Username
+        </label>
+        <input
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="h-10"
+          type="text"
+          id="username"
+          autoComplete="off"
+        />
+      </div>
 
-      <Form.Submit asChild>
+      <div className="flex flex-col mb-4">
+        <label className="text-sm">Password</label>
+        <input
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="h-10"
+          type="password"
+          id="password"
+          autoComplete="off"
+          required
+        />
+      </div>
+      {props.type === 'register' && (
+        <div className="flex flex-col mb-4">
+          <label className="text-sm">Confirm password</label>
+          <input
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="h-10"
+            type="password"
+            id="confirmPassword"
+            autoComplete="off"
+            required
+          />
+        </div>
+      )}
+      <div className="text-center w-full flex flex-col gap-4 justify-around items-center">
         <button className="w-full border border-black h-10 rounded-sm mt-4">
-          {buttonText}
+          {props.type === 'login' ? 'Login' : 'Sign Up'}
         </button>
-      </Form.Submit>
-    </Form.Root>
+        {props.type === 'login' ? (
+          <div>
+            <p>Don't have an account?</p>
+            <Link href={'/register'}>Register</Link>
+          </div>
+        ) : (
+          <div>
+            <p>Already have an account?</p>
+            <Link href={'/'}>Log In</Link>
+          </div>
+        )}
+      </div>
+    </form>
   );
 };
 export default AuthForm;
