95049bc5a8eff1a48549557f11a2f242343f0436
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-07-23 17:52:15 +0100

    Refactor form styles into scss file

--- client/src/app/sandbox/auth/components/AuthForm.tsx
+++ client/src/app/sandbox/auth/components/AuthForm.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
+
+import './_authform.scss';
 
 interface Props {
   type: 'register' | 'login';
@@ -13,32 +15,32 @@ const AuthForm = (props: Props) => {
   const [confirmPassword, setConfirmPassword] = useState<string>('');
 
   return (
-    <form className="border border-black rounded-sm px-6 py-8 max-w-2xl">
-      <h2 className="text-center mb-4">
+    <form className="auth-form">
+      <h2 className="auth-form__title">
         {props.type === 'login' ? 'Login' : 'Sign up for an account'}
       </h2>
-      <div className="flex flex-col mb-4">
-        <label htmlFor="username" className="text-sm">
+      <div className="auth-form__input-group">
+        <label htmlFor="username" className="auth-form__input-label">
           Username
         </label>
         <input
           name="username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          className="h-10"
+          className="auth-form__input-field"
           type="text"
           id="username"
           autoComplete="off"
         />
       </div>
 
-      <div className="flex flex-col mb-4">
-        <label className="text-sm">Password</label>
+      <div className="auth-form__input-group">
+        <label className="auth-form__input-label">Password</label>
         <input
           name="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="h-10"
+          className="auth-form__input-field"
           type="password"
           id="password"
           autoComplete="off"
@@ -46,13 +48,13 @@ const AuthForm = (props: Props) => {
         />
       </div>
       {props.type === 'register' && (
-        <div className="flex flex-col mb-4">
-          <label className="text-sm">Confirm password</label>
+        <div className="auth-form__input-group">
+          <label className="auth-form__input-label">Confirm password</label>
           <input
             name="confirmPassword"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
-            className="h-10"
+            className="auth-form__input-field"
             type="password"
             id="confirmPassword"
             autoComplete="off"
@@ -60,13 +62,13 @@ const AuthForm = (props: Props) => {
           />
         </div>
       )}
-      <div className="text-center w-full flex flex-col gap-4 justify-around items-center">
-        <button className="w-full border border-black h-10 rounded-sm mt-4">
+      <div className="auth-form__text">
+        <button className="auth-form__button">
           {props.type === 'login' ? 'Login' : 'Sign Up'}
         </button>
         {props.type === 'login' ? (
           <div>
-            <p>Don't have an account?</p>
+            <p>Don&apos;t have an account?</p>
             <Link href={'/register'}>Register</Link>
           </div>
         ) : (
