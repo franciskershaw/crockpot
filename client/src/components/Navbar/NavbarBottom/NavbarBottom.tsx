fddc3e8f832447ee74b20d42bb568e4d2ff6c629
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-07-30 17:10:39 +0100

    hide buttons when not logged in

--- client/src/components/Navbar/NavbarBottom/NavbarBottom.tsx
+++ client/src/components/Navbar/NavbarBottom/NavbarBottom.tsx
@@ -1,23 +1,29 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import React from "react";
-import Button from "../../Button/Button";
-import NavbarSharedLinks from "../NavbarSharedLinks/NavbarSharedLinks";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import Button from '../../Button/Button';
+import NavbarSharedLinks from '../NavbarSharedLinks/NavbarSharedLinks';
+import useUser from '@/src/hooks/auth/useUser';
 
 const NavbarBottom = () => {
   const pathname = usePathname();
 
+  const { user } = useUser();
+
   return (
     <nav className="p-4 border-2 border-black bg-white fixed bottom-0 left-0 w-full flex justify-evenly md:hidden">
-      <Link href="/add-recipe">
-        <Button
-          border
-          inverse={pathname.startsWith("/add-recipe")}
-          text="Add Recipe"
-        />
-      </Link>
+      {user && (
+        <Link href="/add-recipe">
+          <Button
+            border
+            inverse={pathname.startsWith('/add-recipe')}
+            text="Add Recipe"
+          />
+        </Link>
+      )}
+
       <NavbarSharedLinks />
     </nav>
   );
--- client/src/components/Navbar/NavbarSharedLinks/NavbarSharedLinks.tsx
+++ client/src/components/Navbar/NavbarSharedLinks/NavbarSharedLinks.tsx
@@ -3,12 +3,13 @@
 import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import useUser from '@/src/hooks/auth/useUser';
 
 import Button from '../../Button/Button';
 
 const NavbarSharedLinks = () => {
   const pathname = usePathname();
-
+  const { user } = useUser();
   return (
     <>
       <Link href="/browse">
@@ -18,13 +19,15 @@ const NavbarSharedLinks = () => {
           text="Browse Recipes"
         />
       </Link>
-      <Link href="/your-crockpot">
-        <Button
-          border
-          inverse={pathname.startsWith('/your-crockpot')}
-          text="Your Crockpot"
-        />
-      </Link>
+      {user && (
+        <Link href="/your-crockpot">
+          <Button
+            border
+            inverse={pathname.startsWith('/your-crockpot')}
+            text="Your Crockpot"
+          />
+        </Link>
+      )}
     </>
   );
 };
