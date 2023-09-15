4d51c0f3ef7017b885819fdca9174fe28103e2f5
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-09-15 22:40:47 +0200

    Animate header toggle

--- client/src/app/your-crockpot/components/HeaderToggle/HeaderToggle.tsx
+++ client/src/app/your-crockpot/components/HeaderToggle/HeaderToggle.tsx
@@ -6,27 +6,30 @@ const HeaderToggle = () => {
   return (
     <Tabs.Root defaultValue="menu">
       <div className="max-w-screen-md mx-auto">
-        <Tabs.List className="flex justify-between bg-gray-200 p-2 rounded-t">
+        <Tabs.List className="flex justify-between bg-gray-200 p-2 rounded-t relative">
           <Tabs.Trigger
             value="menu"
-            className="flex-1 text-center py-2 transition duration-300 ease-in-out"
-            data-active-class="bg-blue-500 text-white"
+            className="flex-1 text-center py-2 transition duration-300 ease-in-out relative"
+            data-active-class="underline-active"
           >
             Menu
+            <span className="underline"></span>
           </Tabs.Trigger>
           <Tabs.Trigger
             value="favourites"
-            className="flex-1 text-center py-2 transition duration-300 ease-in-out"
-            data-active-class="bg-blue-500 text-white"
+            className="flex-1 text-center py-2 transition duration-300 ease-in-out relative"
+            data-active-class="underline-active"
           >
             Favourites
+            <span className="underline"></span>
           </Tabs.Trigger>
           <Tabs.Trigger
             value="my-recipes"
-            className="flex-1 text-center py-2 transition duration-300 ease-in-out"
-            data-active-class="bg-blue-500 text-white"
+            className="flex-1 text-center py-2 transition duration-300 ease-in-out relative"
+            data-active-class="underline-active"
           >
             My Recipes
+            <span className="underline"></span>
           </Tabs.Trigger>
         </Tabs.List>
       </div>
@@ -35,21 +38,18 @@ const HeaderToggle = () => {
         value="menu"
         className="p-4 border rounded-b border-t-0 w-full"
       >
-        {/* Your Menu content here */}
         Menu Content
       </Tabs.Content>
       <Tabs.Content
         value="favourites"
         className="p-4 border rounded-b border-t-0 w-full"
       >
-        {/* Your Favourites content here */}
         Favourites Content
       </Tabs.Content>
       <Tabs.Content
         value="my-recipes"
         className="p-4 border rounded-b border-t-0 w-full"
       >
-        {/* Your My Recipes content here */}
         My Recipes Content
       </Tabs.Content>
     </Tabs.Root>
