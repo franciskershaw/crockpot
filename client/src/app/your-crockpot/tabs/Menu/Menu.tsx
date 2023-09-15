89fffeb633e362bf0db0dc08e0e49f0cea605a00
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-09-15 23:21:07 +0200

    Add shopping list button

--- client/src/app/your-crockpot/tabs/Menu/Menu.tsx
+++ client/src/app/your-crockpot/tabs/Menu/Menu.tsx
@@ -1,5 +1,19 @@
+import Button from "@/src/components/Button/Button";
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="">
+      <div className="tw flex justify-center">
+        <Button
+          type="tertiary"
+          border
+          text="Shopping List"
+          onClick={() => console.log("Hello!")}
+        />
+      </div>
+      <div className="tw">Recipes here</div>
+    </div>
+  );
 };
 
 export default Menu;
