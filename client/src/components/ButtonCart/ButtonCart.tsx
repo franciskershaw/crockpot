b332a655344ddc5d9ebc665b98fabc4782edb020
Zoe Thexton <zoe.thexton@breedr.co>
2023-11-04 20:15:21 +0000

    Add styling and animation to ButtonCart

--- client/src/components/ButtonCart/ButtonCart.tsx
+++ client/src/components/ButtonCart/ButtonCart.tsx
@@ -1,37 +1,100 @@
-import { RiShoppingBasketLine } from "react-icons/ri";
-import * as Toggle from "@radix-ui/react-toggle";
-import { useState } from "react";
-import Icon from "../Icon/Icon";
-import QuantityInput from "../QuantityInput/QuantityInput";
+import { useState } from 'react';
+import Icon from '../Icon/Icon';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import { RiShoppingBasketLine } from 'react-icons/ri';
 
 type ButtonCartProps = {
-  recipeId: string;
+	recipeId: string;
+	initialValue?: number;
+	min?: number;
+	max?: number;
+	onChange?: (value: number) => void;
 };
 
-const ButtonCart = ({ recipeId }: ButtonCartProps) => {
-  const [isPressed, setIsPressed] = useState(false);
+const ButtonCart = ({
+	recipeId,
+	initialValue = 4,
+	min = 0,
+	max = 50,
+	onChange,
+}: ButtonCartProps) => {
+	const [isExpanded, setIsExpanded] = useState(false);
+	const [quantity, setQuantity] = useState(initialValue);
 
-  const handleCartChange = (pressed: boolean) => {
-    setIsPressed(pressed);
+	const updateQuantity = (newQuantity: number) => {
+		if (newQuantity !== quantity) {
+			setQuantity(newQuantity);
+			if (onChange) {
+				onChange(newQuantity);
+			}
+		}
+	};
 
-    console.log(pressed, recipeId);
-  };
+	const handleDecrease = () => {
+		updateQuantity(Math.max(min, quantity - 1));
+	};
 
-  const handleQuantityChange = (value: number) => {
-    console.log("Quantity changed:", value);
-  };
+	const handleIncrease = () => {
+		updateQuantity(Math.min(max, quantity + 1));
+	};
 
-  return (
-    <Toggle.Root onPressedChange={handleCartChange} className="cursor-pointer">
-      {isPressed ? (
-        <QuantityInput initialValue={10} onChange={handleQuantityChange} />
-      ) : (
-        <Icon border active={isPressed} size="lg">
-          <RiShoppingBasketLine />
-        </Icon>
-      )}
-    </Toggle.Root>
-  );
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const newValue = parseInt(event.target.value, 10);
+		if (!isNaN(newValue)) {
+			updateQuantity(Math.max(min, Math.min(max, newValue)));
+		}
+	};
+
+	return (
+		<div className="relative">
+			<div className="flex items-center border-2 border-blue-500 rounded-full overflow-hidden w-fit mb-5">
+				<div className="relative">
+					<div
+						onClick={() => setIsExpanded(true)}
+						className={`absolute opacity-100 cursor-pointer fade ${
+							isExpanded ? '!opacity-0 -z-10' : ''
+						}`}
+					>
+						<Icon size="lg" type="secondary">
+							<RiShoppingBasketLine />
+						</Icon>
+					</div>
+					<div className="cursor-pointer" onClick={handleDecrease}>
+						<Icon size="lg" type="secondary" active>
+							<AiOutlineMinus />
+						</Icon>
+					</div>
+				</div>
+
+				<div
+					className={`quantity-input-container ${
+						isExpanded ? 'quantity-input-container--expanded' : ''
+					}`}
+				>
+					<input
+						type="number"
+						value={quantity}
+						min={min}
+						max={max}
+						onChange={handleChange}
+					/>
+					<div className="cursor-pointer" onClick={handleIncrease}>
+						<Icon size="lg" type="secondary" active>
+							<AiOutlinePlus />
+						</Icon>
+					</div>
+				</div>
+			</div>
+			{isExpanded && (
+				<h5
+					onClick={() => setIsExpanded(false)}
+					className="absolute bottom-[-20px] w-[134px] text-center underline cursor-pointer"
+				>
+					Remove from basket
+				</h5>
+			)}
+		</div>
+	);
 };
 
 export default ButtonCart;
--- client/src/components/QuantityInput/QuantityInput.tsx
+++ client/src/components/QuantityInput/QuantityInput.tsx
@@ -6,61 +6,61 @@ import Button from '../Button/Button';
 import './styles.scss';
 
 type QuantityInputProps = {
-  initialValue: number;
-  min?: number;
-  max?: number;
-  onChange?: (value: number) => void;
+	initialValue: number;
+	min?: number;
+	max?: number;
+	onChange?: (value: number) => void;
 };
 
 const QuantityInput = ({
-  initialValue,
-  min = 0,
-  max = 100,
-  onChange,
+	initialValue,
+	min = 0,
+	max = 100,
+	onChange,
 }: QuantityInputProps) => {
-  const [quantity, setQuantity] = useState(initialValue);
+	const [quantity, setQuantity] = useState(initialValue);
 
-  const handleDecrease = () => {
-    const newValue = Math.max(min, quantity - 1);
-    setQuantity(newValue);
-    if (onChange) {
-      onChange(newValue);
-    }
-  };
+	const handleDecrease = () => {
+		const newValue = Math.max(min, quantity - 1);
+		setQuantity(newValue);
+		if (onChange) {
+			onChange(newValue);
+		}
+	};
 
-  const handleIncrease = () => {
-    const newValue = Math.min(max, quantity + 1);
-    setQuantity(newValue);
-    if (onChange) {
-      onChange(newValue);
-    }
-  };
+	const handleIncrease = () => {
+		const newValue = Math.min(max, quantity + 1);
+		setQuantity(newValue);
+		if (onChange) {
+			onChange(newValue);
+		}
+	};
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = parseInt(event.target.value, 10);
-    const clampedValue = Math.max(min, Math.min(max, newValue));
-    setQuantity(clampedValue);
-    if (onChange) {
-      onChange(clampedValue);
-    }
-  };
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const newValue = parseInt(event.target.value, 10);
+		const clampedValue = Math.max(min, Math.min(max, newValue));
+		setQuantity(clampedValue);
+		if (onChange) {
+			onChange(clampedValue);
+		}
+	};
 
-  return (
-    <div className="quantity-input bg-white">
-      <Button inverse type="secondary" onClick={handleDecrease}>
-        <AiOutlineMinus />
-      </Button>
-      <input
-        className="text-center w-[50px] bg-transparent"
-        type="number"
-        value={quantity}
-        onChange={handleChange}
-      />
-      <Button inverse type="secondary" onClick={handleIncrease}>
-        <AiOutlinePlus />
-      </Button>
-    </div>
-  );
+	return (
+		<div className="flex border-2 border-black w-fit rounded-full bg-white">
+			<Button inverse type="secondary" onClick={handleDecrease}>
+				<AiOutlineMinus />
+			</Button>
+			<input
+				className="text-center w-[50px] bg-transparent"
+				type="number"
+				value={quantity}
+				onChange={handleChange}
+			/>
+			<Button inverse type="secondary" onClick={handleIncrease}>
+				<AiOutlinePlus />
+			</Button>
+		</div>
+	);
 };
 
 export default QuantityInput;
