d76b48d47a699fc440b58b9039657b4623ef0a2d
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-11-05 15:14:49 +0000

    Add extra item from search results

--- client/src/app/your-crockpot/components/ShoppingList/ShoppingList.tsx
+++ client/src/app/your-crockpot/components/ShoppingList/ShoppingList.tsx
@@ -1,5 +1,5 @@
 import SearchBar from '@/src/components/FormSearchBar/SearchBar';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import Accordion from '@/src/components/Accordion/Accordion';
 import useShoppingList from '../../hooks/useShoppingList';
 import useExtraItems from '../../hooks/useExtraItems';
@@ -8,12 +8,20 @@ import Icon from '@/src/components/Icon/Icon';
 import iconMapping from '@/src/components/Icon/iconMapping';
 import ShoppingListItem from './ShoppingListItem';
 import Button from '@/src/components/Button/Button';
+import useItems from '@/src/hooks/items/useItems';
+import Modal from '@/src/components/Modal/Modal';
+import QuantityInput from '@/src/components/QuantityInput/QuantityInput';
+import { Item } from '@/src/types/types';
 
 const ShoppingList = () => {
   const [searchQuery, setSearchQuery] = useState('');
+  const [extraQuantity, setExtraQuantity] = useState(0);
+  const [extraUnit, setExtraUnit] = useState('');
 
   const { groupedShoppingList } = useShoppingList();
-  const { clearExtraItems } = useExtraItems();
+  const { clearExtraItems, updateExtraItems } = useExtraItems();
+
+  const { filterItems, allItems } = useItems();
 
   const accordionItems = useMemo(() => {
     return groupedShoppingList?.map((category) => {
@@ -42,16 +50,80 @@ const ShoppingList = () => {
     });
   }, [groupedShoppingList]);
 
+  const searchResults = useMemo(() => {
+    return filterItems(allItems, searchQuery);
+  }, [allItems, searchQuery, filterItems]);
+
+  const handleAddExtraItem = (item: Item) => {
+    if (extraQuantity > 0) {
+      updateExtraItems({
+        itemId: item._id,
+        body: {
+          quantity: extraQuantity,
+          unit: extraUnit,
+        },
+      });
+    }
+  };
+
   return (
     <div className="flex flex-col">
       <h2 className="w-3/4 font-bold pt-3">Shopping List</h2>
       <div className="flex items-end justify-between">
         {/* header and search */}
-        <SearchBar
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
-          label="Search Extra Items"
-        />
+        <div className="relative">
+          <SearchBar
+            searchQuery={searchQuery}
+            setSearchQuery={setSearchQuery}
+            label="Search Extra Items"
+          />
+          {searchResults.length ? (
+            <div className="absolute top-full left-0 z-10 w-full bg-white border border-gray-300 shadow-lg">
+              {searchResults.map((result) => (
+                <Modal
+                  key={result._id}
+                  title={`Add extra ${result.name} to shopping list`}
+                  trigger={
+                    <p onClick={() => console.log('HI')}>{result.name}</p>
+                  }>
+                  <div className="flex flex-col items-center gap-4 p-8">
+                    <div className="flex items-center justify-center gap-8 flex-grow mb-4">
+                      <div className="flex flex-col gap-2">
+                        <label htmlFor="">Amount</label>
+                        <QuantityInput
+                          value={extraQuantity}
+                          setValue={setExtraQuantity}
+                        />
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <label htmlFor="">Unit (if applicable)</label>
+                        <select
+                          className="border text-xl w-full py-1"
+                          name=""
+                          id=""
+                          onChange={(e) => setExtraUnit(e.target.value)}
+                          value={extraUnit}>
+                          <option value="">-</option>
+                          <option value="cans">Cans</option>
+                          <option value="g">g</option>
+                          <option value="ml">ml</option>
+                          <option value="tbsp">Tablespoons</option>
+                          <option value="tsp">Teaspoons</option>
+                        </select>
+                      </div>
+                    </div>
+                    <Button
+                      onClick={() => handleAddExtraItem(result)}
+                      inverse
+                      text="Add to Shopping List"
+                    />
+                  </div>
+                </Modal>
+              ))}
+            </div>
+          ) : null}
+        </div>
+
         <Button onClick={() => clearExtraItems()} border text="Reset to menu" />
       </div>
       {/* Shopping list */}
--- client/src/components/QuantityInput/QuantityInput.tsx
+++ client/src/components/QuantityInput/QuantityInput.tsx
@@ -4,62 +4,56 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 import Button from '../Button/Button';
 
-type QuantityInputProps = {
-	initialValue: number;
-	min?: number;
-	max?: number;
-	onChange?: (value: number) => void;
-};
+interface QuantityInputProps {
+  value: number;
+  setValue: React.Dispatch<React.SetStateAction<number>>;
+  min?: number;
+  max?: number;
+  onChange?: (value: number) => void;
+}
 
 const QuantityInput = ({
-	initialValue,
-	min = 0,
-	max = 100,
-	onChange,
+  value,
+  setValue,
+  min = 0,
+  max = 100,
+  onChange,
 }: QuantityInputProps) => {
-	const [quantity, setQuantity] = useState(initialValue);
+  const handleDecrease = () => {
+    const newValue = Math.max(min, value - 1);
+    setValue(newValue);
+    if (onChange) onChange(newValue);
+  };
 
-	const handleDecrease = () => {
-		const newValue = Math.max(min, quantity - 1);
-		setQuantity(newValue);
-		if (onChange) {
-			onChange(newValue);
-		}
-	};
+  const handleIncrease = () => {
+    const newValue = Math.min(max, value + 1);
+    setValue(newValue);
+    if (onChange) onChange(newValue);
+  };
 
-	const handleIncrease = () => {
-		const newValue = Math.min(max, quantity + 1);
-		setQuantity(newValue);
-		if (onChange) {
-			onChange(newValue);
-		}
-	};
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(event.target.value, 10);
+    const clampedValue = Math.max(min, Math.min(max, newValue));
+    setValue(clampedValue);
+    if (onChange) onChange(clampedValue);
+  };
 
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const newValue = parseInt(event.target.value, 10);
-		const clampedValue = Math.max(min, Math.min(max, newValue));
-		setQuantity(clampedValue);
-		if (onChange) {
-			onChange(clampedValue);
-		}
-	};
-
-	return (
-		<div className="flex border-2 border-black w-fit rounded-full bg-white">
-			<Button inverse type="secondary" onClick={handleDecrease}>
-				<AiOutlineMinus />
-			</Button>
-			<input
-				className="text-center w-[50px] bg-transparent"
-				type="number"
-				value={quantity}
-				onChange={handleChange}
-			/>
-			<Button inverse type="secondary" onClick={handleIncrease}>
-				<AiOutlinePlus />
-			</Button>
-		</div>
-	);
+  return (
+    <div className="flex border-2 border-black w-fit rounded-full bg-white">
+      <Button inverse type="secondary" onClick={handleDecrease}>
+        <AiOutlineMinus />
+      </Button>
+      <input
+        className="text-center w-[50px] bg-transparent"
+        type="number"
+        value={value}
+        onChange={handleChange}
+      />
+      <Button inverse type="secondary" onClick={handleIncrease}>
+        <AiOutlinePlus />
+      </Button>
+    </div>
+  );
 };
 
 export default QuantityInput;
