5175e560d0a985a747c76e9edbd804be4ec73f26
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-11-12 10:22:54 +0000

    Refactor and fix useFavourites

--- client/src/hooks/users/useFavourites.tsx
+++ client/src/hooks/users/useFavourites.tsx
@@ -1,8 +1,8 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import useAxios from "../axios/useAxios";
-import useUser from "../auth/useUser";
-import { createConfig } from "@/src/helper";
-import { queryKeys } from "@/src/providers/Providers";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import useAxios from '../axios/useAxios';
+import useUser from '../auth/useUser';
+import { createConfig } from '@/src/helper';
+import { queryKeys } from '@/src/providers/Providers';
 
 type FavouriteVariables = {
   _id: string;
@@ -16,10 +16,10 @@ const useFavourites = () => {
   // Requests
   const getFavouriteRecipesReq = async () => {
     if (!user) {
-      throw new Error("User is not authenticated");
+      throw new Error('User is not authenticated');
     }
     const config = createConfig(user);
-    const response = await api.get("/api/users/favourites", config);
+    const response = await api.get('/api/users/favourites', config);
     return response.data;
   };
 
@@ -32,29 +32,29 @@ const useFavourites = () => {
   );
 
   // useMutation hook
-  const toggleFavouriteReq = async (_id: string) => {
+  const toggleFavouriteReq = async ({ _id }: FavouriteVariables) => {
     if (!user) {
-      throw new Error("User is not authenticated");
+      throw new Error('User is not authenticated');
     }
     const config = createConfig(user);
-    const response = await api.put(
+    await api.put(
       `/api/users/favourites`,
       {
         _id,
       },
       config
     );
-    return response.data;
   };
 
   const { mutate: toggleFavourite } = useMutation(
-    (variables: FavouriteVariables) => toggleFavouriteReq(variables._id),
+    (variables: FavouriteVariables) => toggleFavouriteReq(variables),
     {
       onSuccess: () => {
         queryClient.invalidateQueries([queryKeys.user]);
+        queryClient.invalidateQueries([queryKeys.favouriteRecipes]);
       },
       onError: (error) => {
-        console.error("Error toggling recipe favourite status:", error);
+        console.error('Error toggling recipe favourite status:', error);
       },
     }
   );
