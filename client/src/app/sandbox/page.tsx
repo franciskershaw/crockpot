58fbd2f58664ed87bf2d8b1921d22c8593968dd1
Zoe Thexton <zoe.thexton@breedr.co>
2023-11-23 20:37:57 +0000

    Add styling

--- client/src/app/sandbox/page.tsx
+++ client/src/app/sandbox/page.tsx
@@ -1,91 +1,83 @@
-"use client";
+'use client';
 
-import Checkbox from "@/src/components/Checkbox/Checkbox";
-import Slider from "@/src/components/Slider/Slider";
-import Switch from "@/src/components/Switch/Switch";
-import Accordion from "@/src/components/Accordion/Accordion";
-import { FC } from "react";
-import Icon from "@/src/components/Icon/Icon";
-import { AiFillFilter, AiOutlineSearch } from "react-icons/ai";
-import ButtonFav from "@/src/components/ButtonFav/ButtonFav";
-import ButtonCart from "@/src/components/ButtonCart/ButtonCart";
-import Button from "@/src/components/Button/Button";
-import QuantityInput from "@/src/components/QuantityInput/QuantityInput";
-import RecipeCard from "@/src/components/RecipeCard/RecipeCard";
-import SearchBar from "@/src/components/FormSearchBar/SearchBar";
-import Modal from "@/src/components/Modal/Modal";
+import Checkbox from '@/src/components/Checkbox/Checkbox';
+import Slider from '@/src/components/Slider/Slider';
+import Switch from '@/src/components/Switch/Switch';
+import Accordion from '@/src/components/Accordion/Accordion';
+import { FC } from 'react';
+import Icon from '@/src/components/Icon/Icon';
+import { AiFillFilter, AiOutlineSearch } from 'react-icons/ai';
+import ButtonFav from '@/src/components/ButtonFav/ButtonFav';
+import ButtonCart from '@/src/components/ButtonCart/ButtonCart';
+import Button from '@/src/components/Button/Button';
+import QuantityInput from '@/src/components/QuantityInput/QuantityInput';
+import RecipeCard from '@/src/components/RecipeCard/RecipeCard';
+import SearchBar from '@/src/components/FormSearchBar/SearchBar';
+import Modal from '@/src/components/Modal/Modal';
 
 const SandBoxPage: FC = () => {
-  const accordionItems = [
-    {
-      heading: "Heading 1",
-      children:
-        "Bacon ipsum dolor amet chislic prosciutto biltong chicken shoulder swine salami rump alcatra bresaola, tenderloin cow andouille beef. Meatball tri-tip chicken leberkas. Beef landjaeger chuck ham hock. Jowl flank landjaeger ground round, prosciutto tri-tip ribeye meatball cupim buffalo pastrami. Buffalo sausage fatback picanha strip steak alcatra filet mignon pancetta ham hock flank frankfurter pastrami burgdoggen short ribs prosciutto.",
-    },
-    {
-      heading: "Heading 2",
-      children: <p>Paragraph paragraph!</p>,
-    },
-    {
-      heading: "Heading 3",
-      children: (
-        <Switch
-          label={"Switch"}
-          onChange={(values: boolean) => console.log(values)}
-        />
-      ),
-    },
-  ];
+	const accordionItems = [
+		{
+			heading: 'Heading 1',
+			children:
+				'Bacon ipsum dolor amet chislic prosciutto biltong chicken shoulder swine salami rump alcatra bresaola, tenderloin cow andouille beef. Meatball tri-tip chicken leberkas. Beef landjaeger chuck ham hock. Jowl flank landjaeger ground round, prosciutto tri-tip ribeye meatball cupim buffalo pastrami. Buffalo sausage fatback picanha strip steak alcatra filet mignon pancetta ham hock flank frankfurter pastrami burgdoggen short ribs prosciutto.',
+		},
+		{
+			heading: 'Heading 2',
+			children: <p>Paragraph paragraph!</p>,
+		},
+	];
 
-  const handleQuantityChange = (value: number) => {
-    console.log("Quantity changed:", value);
-  };
+	const handleQuantityChange = (value: number) => {
+		console.log('Quantity changed:', value);
+	};
 
-  return (
-    <div className="space-y-2">
-      <div className="container border-2 border-black">
-        <h2>Container</h2>
-      </div>
-      <div className="container container--sm border-2 border-black">
-        <h2>Container sm</h2>
-      </div>
-      <div className="container container--md border-2 border-black">
-        <h2>Container md</h2>
-      </div>
-      <div className="container container--lg border-2 border-black">
-        <h2>Container lg</h2>
-      </div>
-      <div className="container container--full border-2 border-black">
-        <h2>Container full</h2>
-      </div>
-      <div className="container">
-        <h1>Crockpot</h1>
-        <h2>Sandbox Page!</h2>
-        <h3>Sandbox Page!</h3>
-        <h4>Sandbox Page!</h4>
-        <h5>Sandbox Page!</h5>
-        <h6>Sandbox Page!</h6>
-        <p>Sandbox Page! Sandbox Page! Sandbox Page!</p>
-        <a href="">Sandbox Page!</a>
-      </div>
-      <div className="container flex">
-        <div className="h-10 w-10 rounded-full bg-primary-light"></div>
-        <div className="h-10 w-10 rounded-full bg-primary"></div>
-        <div className="h-10 w-10 rounded-full bg-primary-dark"></div>
-        <div className="h-10 w-10 rounded-full bg-secondary-light"></div>
-        <div className="h-10 w-10 rounded-full bg-secondary"></div>
-        <div className="h-10 w-10 rounded-full bg-secondary-dark"></div>
-        <div className="h-10 w-10 rounded-full bg-black"></div>
-        <div className="h-10 w-10 rounded-full bg-white border-black border-2"></div>
-      </div>
-      <div className="container pt-8">
+	return (
+		<div className="space-y-2">
+			<div className="container border-2 border-black">
+				<h2>Container</h2>
+			</div>
+			<div className="container container--sm border-2 border-black">
+				<h2>Container sm</h2>
+			</div>
+			<div className="container container--md border-2 border-black">
+				<h2>Container md</h2>
+			</div>
+			<div className="container container--lg border-2 border-black">
+				<h2>Container lg</h2>
+			</div>
+			<div className="container container--full border-2 border-black">
+				<h2>Container full</h2>
+			</div>
+			<div className="container">
+				<h1>Crockpot</h1>
+				<h2>Sandbox Page!</h2>
+				<h3>Sandbox Page!</h3>
+				<h4>Sandbox Page!</h4>
+				<h5>Sandbox Page!</h5>
+				<h6>Sandbox Page!</h6>
+				<p>Sandbox Page! Sandbox Page! Sandbox Page!</p>
+				<a href="">Sandbox Page!</a>
+			</div>
+			<div className="container flex">
+				<div className="h-10 w-10 rounded-full bg-primary"></div>
+				<div className="h-10 w-10 rounded-full bg-secondary"></div>
+				<div className="h-10 w-10 rounded-full bg-yellow"></div>
+				<div className="h-10 w-10 rounded-full bg-white border border-black"></div>
+				<div className="h-10 w-10 rounded-full bg-blue-light"></div>
+				<div className="h-10 w-10 rounded-full bg-disabled"></div>
+				<div className="h-10 w-10 rounded-full bg-body-light border border-black"></div>
+				<div className="h-10 w-10 rounded-full bg-body"></div>
+				<div className="h-10 w-10 rounded-full bg-black"></div>
+			</div>
+			{/* <div className="container pt-8">
         <Slider
           min={25}
           max={75}
           onChange={(values: number[]) => console.log(values)}
         />
-      </div>
-      <div className="container pt-8">
+      </div> */}
+			{/* <div className="container pt-8">
         <Switch
           label={"Switch"}
           onChange={(values: boolean) => console.log(values)}
@@ -104,104 +96,104 @@ const SandBoxPage: FC = () => {
           label={"Checkbox"}
           onChange={(values: boolean) => console.log(values)}
         />
-      </div>
-      <div className="container pt-8 space-y-2">
-        <Accordion items={accordionItems} />
-      </div>
-      <div className="container pt-8 flex space-x-2">
-        <Icon border size="sm">
-          <AiOutlineSearch />
-        </Icon>
-        <Icon border>
-          <AiOutlineSearch />
-        </Icon>
-        <Icon border active size="lg">
-          <AiOutlineSearch />
-        </Icon>
-        <Icon border type="secondary" size="sm">
-          <AiOutlineSearch />
-        </Icon>
-        <Icon border type="secondary">
-          <AiOutlineSearch />
-        </Icon>
-        <Icon border active type="secondary" size="lg">
-          <AiOutlineSearch />
-        </Icon>
-        <Icon border type="tertiary" size="sm">
-          <AiOutlineSearch />
-        </Icon>
-        <Icon border type="tertiary">
-          <AiOutlineSearch />
-        </Icon>
-        <Icon border active type="tertiary" size="lg">
-          <AiOutlineSearch />
-        </Icon>
-      </div>
-      <div className="container pt-8 space-x-2">
-        <ButtonFav recipeId="halluomi-tacos" />
-        <ButtonCart recipeId="sheperds-pie" />
-        <div className="flex space-x-2">
-          <Button border onClick={() => console.log("Hello!")}>
-            <AiOutlineSearch />
-          </Button>
-          <Button
-            border
-            text="Button"
-            onClick={() => console.log("Hello!")}
-          ></Button>
-          <Button
-            border
-            text="Button"
-            inverse
-            onClick={() => console.log("Hello!")}
-          >
-            <AiOutlineSearch />
-          </Button>
-        </div>
-        <div className="flex space-x-2">
-          <Button type="secondary" border onClick={() => console.log("Hello!")}>
-            <AiOutlineSearch />
-          </Button>
-          <Button
-            border
-            text="Button"
-            onClick={() => console.log("Hello!")}
-            type="secondary"
-          ></Button>
-          <Button
-            type="secondary"
-            border
-            inverse
-            text="Button"
-            onClick={() => console.log("Hello!")}
-          >
-            <AiOutlineSearch />
-          </Button>
-        </div>
-        <div className="flex space-x-2">
-          <Button type="tertiary" border onClick={() => console.log("Hello!")}>
-            <AiOutlineSearch />
-          </Button>
-          <Button
-            type="tertiary"
-            border
-            text="Button"
-            onClick={() => console.log("Hello!")}
-          ></Button>
-          <Button
-            type="tertiary"
-            border
-            inverse
-            text="Button"
-            onClick={() => console.log("Hello!")}
-          >
-            <AiOutlineSearch />
-          </Button>
-        </div>
-        <QuantityInput initialValue={10} onChange={handleQuantityChange} />
-      </div>
-      <div className="container">
-        <div className="grid grid-cols-2 gap-5">
+      </div> */}
+			<div className="container pt-8 space-y-2">
+				<Accordion items={accordionItems} />
+			</div>
+			<div className="container pt-8 flex space-x-2">
+				<Icon border size="sm">
+					<AiOutlineSearch />
+				</Icon>
+				<Icon border>
+					<AiOutlineSearch />
+				</Icon>
+				<Icon border active size="lg">
+					<AiOutlineSearch />
+				</Icon>
+				<Icon border type="secondary" size="sm">
+					<AiOutlineSearch />
+				</Icon>
+				<Icon border type="secondary">
+					<AiOutlineSearch />
+				</Icon>
+				<Icon border active type="secondary" size="lg">
+					<AiOutlineSearch />
+				</Icon>
+				<Icon border type="tertiary" size="sm">
+					<AiOutlineSearch />
+				</Icon>
+				<Icon border type="tertiary">
+					<AiOutlineSearch />
+				</Icon>
+				<Icon border active type="tertiary" size="lg">
+					<AiOutlineSearch />
+				</Icon>
+			</div>
+			<div className="container pt-8 space-x-2">
+				{/* <ButtonFav recipeId="halluomi-tacos" /> */}
+				{/* <ButtonCart recipeId="sheperds-pie" /> */}
+				<div className="flex space-x-2">
+					<Button border onClick={() => console.log('Hello!')}>
+						<AiOutlineSearch />
+					</Button>
+					<Button
+						border
+						text="Button"
+						onClick={() => console.log('Hello!')}
+					></Button>
+					<Button
+						border
+						text="Button"
+						inverse
+						onClick={() => console.log('Hello!')}
+					>
+						<AiOutlineSearch />
+					</Button>
+				</div>
+				<div className="flex space-x-2">
+					<Button type="secondary" border onClick={() => console.log('Hello!')}>
+						<AiOutlineSearch />
+					</Button>
+					<Button
+						border
+						text="Button"
+						onClick={() => console.log('Hello!')}
+						type="secondary"
+					></Button>
+					<Button
+						type="secondary"
+						border
+						inverse
+						text="Button"
+						onClick={() => console.log('Hello!')}
+					>
+						<AiOutlineSearch />
+					</Button>
+				</div>
+				<div className="flex space-x-2">
+					<Button type="tertiary" border onClick={() => console.log('Hello!')}>
+						<AiOutlineSearch />
+					</Button>
+					<Button
+						type="tertiary"
+						border
+						text="Button"
+						onClick={() => console.log('Hello!')}
+					></Button>
+					<Button
+						type="tertiary"
+						border
+						inverse
+						text="Button"
+						onClick={() => console.log('Hello!')}
+					>
+						<AiOutlineSearch />
+					</Button>
+				</div>
+				{/* <QuantityInput initialValue={10} onChange={handleQuantityChange} /> */}
+			</div>
+			<div className="container">
+				{/* <div className="grid grid-cols-2 gap-5">
           <RecipeCard
             imageUrl="https://img.hellofresh.com/c_fit,f_auto,fl_lossy,h_1100,q_30,w_2600/hellofresh_s3/image/553654b06ced6ebf798b4567.jpg"
             cookingTime={20}
@@ -220,12 +212,12 @@ const SandBoxPage: FC = () => {
             recipeName="Strolling Rigatoni with Cherry Tomatoes and Mozzarella"
             categories={["Delicious", "Healthy", "Steamy"]}
           />
-        </div>
-        {/* <div>
+        </div> */}
+				{/* <div>
           <SearchBar label="Label!" placeholder="Hello!" />
         </div> */}
-      </div>
-      <div className="container">
+			</div>
+			{/* <div className="container">
       <Modal 
         title="My Modal Title" 
         trigger={
@@ -235,8 +227,8 @@ const SandBoxPage: FC = () => {
           }
         children={<div>This is my modal content.</div>}
       />
-      </div>
-    </div>
-  );
+      </div> */}
+		</div>
+	);
 };
 export default SandBoxPage;
--- client/src/components/Accordion/Accordion.tsx
+++ client/src/components/Accordion/Accordion.tsx
@@ -11,50 +11,51 @@ import './styles.scss';
 import { useState } from 'react';
 
 type AccordionItemProps = {
-  heading: React.ReactNode;
-  children: React.ReactNode;
+	heading: React.ReactNode;
+	children: React.ReactNode;
 };
 
 const AccordionItem = ({ heading, children }: AccordionItemProps) => {
-  const value = React.useMemo(() => uuidv4(), []);
+	const value = React.useMemo(() => uuidv4(), []);
 
-  return (
-    <AccordionRadix.Item value={value} className="border-t border-black py-3">
-      <AccordionRadix.Header>
-        <AccordionRadix.Trigger className="AccordionTrigger flex justify-between items-center w-full">
-          {heading}
-          <div className="AccordionChevron">
-            <Icon>
-              <BsChevronDown />
-            </Icon>
-          </div>
-        </AccordionRadix.Trigger>
-      </AccordionRadix.Header>
-      <AccordionRadix.Content className="AccordionContent pt-2">
-        {children}
-      </AccordionRadix.Content>
-    </AccordionRadix.Item>
-  );
+	return (
+		<AccordionRadix.Item value={value} className="border-t border-black py-3">
+			<AccordionRadix.Header>
+				<AccordionRadix.Trigger className="AccordionTrigger flex justify-between items-center w-full">
+					{heading}
+					<div className="AccordionChevron">
+						<Icon>
+							<BsChevronDown />
+						</Icon>
+					</div>
+				</AccordionRadix.Trigger>
+			</AccordionRadix.Header>
+			<AccordionRadix.Content className="AccordionContent pt-2">
+				{children}
+			</AccordionRadix.Content>
+		</AccordionRadix.Item>
+	);
 };
 
 interface AccordionProps {
-  items: AccordionItemProps[];
+	items: AccordionItemProps[];
 }
 
 const Accordion = ({ items }: AccordionProps) => {
-  const [openItems, setOpenItems] = useState<string[]>([]);
-  return (
-    <AccordionRadix.Root
-      type="multiple"
-      value={openItems}
-      onValueChange={setOpenItems}>
-      {items.map((item, index) => (
-        <AccordionItem key={index} heading={item.heading}>
-          {item.children}
-        </AccordionItem>
-      ))}
-    </AccordionRadix.Root>
-  );
+	const [openItems, setOpenItems] = useState<string[]>([]);
+	return (
+		<AccordionRadix.Root
+			type="multiple"
+			value={openItems}
+			onValueChange={setOpenItems}
+		>
+			{items.map((item, index) => (
+				<AccordionItem key={index} heading={item.heading}>
+					{item.children}
+				</AccordionItem>
+			))}
+		</AccordionRadix.Root>
+	);
 };
 
 export default Accordion;
