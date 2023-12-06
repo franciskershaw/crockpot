41ac2225f5b311fabe0498dab898c4755f58bdfb
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-12-06 19:47:15 +0000

    Add basic security

--- api/server.js
+++ api/server.js
@@ -5,6 +5,8 @@ require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 const PORT = process.env.PORT || 5000;
 
@@ -14,6 +16,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use(helmet());
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+});
+
+app.use(limiter);
 
 const corsOrigin =
 	process.env.NODE_ENV === 'production'
