ba6765b02b150b0211b2511c05e9691b2297cd1d
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-12-12 21:10:16 +0000

    Remove onClick for add recipe button

--- client/src/components/ButtonAddRecipe/ButtonAddRecipe.tsx
+++ client/src/components/ButtonAddRecipe/ButtonAddRecipe.tsx
@@ -6,16 +6,11 @@ import Modal from '../Modal/Modal';
 import AddRecipe from '../AddRecipe/AddRecipe';
 
 function ButtonAddRecipe() {
-	const handlePress = () => {
-		console.log('Hello!');
-	};
-
 	return (
 		<Modal
 			trigger={
 				<button
 					className='btn btn--primary btn--border fixed bottom-20 md:bottom-0 right-0 z-10 m-4'
-					onClick={handlePress}
 					aria-label='Add Recipe'
 				>
 					<Icon size='xl'>
