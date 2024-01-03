46c4d58b05e14d703d8488cc9c692c599669c1cb
Zoe Thexton <zoe.thexton@breedr.co>
2024-01-03 21:21:54 +0000

    Add empty states for favourites

--- client/src/app/your-crockpot/tabs/Favourites/Favourites.tsx
+++ client/src/app/your-crockpot/tabs/Favourites/Favourites.tsx
@@ -1,15 +1,28 @@
 import RecipeCardList from '@/src/components/RecipeCardList/RecipeCardList';
+import useRecipes from '@/src/hooks/recipes/useRecipes';
 import useFavourites from '@/src/hooks/users/useFavourites';
 
 const Favourites = () => {
+	const { allRecipes } = useRecipes();
 	const { favouriteRecipes } = useFavourites();
+
+	const suggestedRecipes = allRecipes.slice(0, 4);
 
 	return (
 		<div className="pt-4 px-4">
 			{favouriteRecipes.length !== 0 ? (
 				<RecipeCardList recipes={favouriteRecipes} fullWidth />
 			) : (
-				<h2>YOUVE GOT NAY RECIPES</h2>
+				<>
+					<div className="text-center mb-4">
+						<h2 className="mb-2">No favourites?</h2>
+						<p className="h3 !leading-5">
+							How about some of these? Click the heart button to add them to
+							your list.
+						</p>
+					</div>
+					<RecipeCardList recipes={suggestedRecipes} fullWidth />
+				</>
 			)}
 		</div>
 	);
