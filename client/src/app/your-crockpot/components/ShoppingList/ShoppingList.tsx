f40a208dcfd139eff477c96baec7ebb176818629
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-01-11 22:58:32 +0000

    Hide clear menu button when shopping list is empty

--- client/src/app/your-crockpot/components/ShoppingList/ShoppingList.tsx
+++ client/src/app/your-crockpot/components/ShoppingList/ShoppingList.tsx
@@ -3,6 +3,7 @@ import { useState, useMemo } from 'react';
 import Accordion from '@/src/components/Accordion/Accordion';
 import useShoppingList from '../../hooks/useShoppingList';
 import useExtraItems from '../../hooks/useExtraItems';
+import useRecipeMenu from '../../hooks/useRecipeMenu';
 import { FaQuestion } from 'react-icons/fa';
 import Icon from '@/src/components/Icon/Icon';
 import iconMapping from '@/src/components/Icon/iconMapping';
@@ -23,16 +24,18 @@ const ShoppingList = () => {
 
 	const { filterItems, allItems } = useItems();
 
+	const { recipeMenu } = useRecipeMenu();
+
 	const accordionItems = useMemo(() => {
 		return groupedShoppingList?.map((category) => {
 			const IconComponent = iconMapping[category.faIcon] || FaQuestion;
 			return {
 				heading: (
-					<div className="flex overflow-hidden">
-						<Icon type="primary">
+					<div className='flex overflow-hidden'>
+						<Icon type='primary'>
 							<IconComponent />
 						</Icon>
-						<span className="h3 text-left pl-2 pr-1 truncate">
+						<span className='h3 text-left pl-2 pr-1 truncate'>
 							{category.categoryName}
 						</span>
 						<span>
@@ -42,7 +45,7 @@ const ShoppingList = () => {
 					</div>
 				),
 				children: (
-					<div className="space-y-0.5">
+					<div className='space-y-0.5'>
 						{category.items.map((item) => (
 							<ShoppingListItem
 								key={`${item.item._id}_${item.quantity}_${item.unit}`}
@@ -75,55 +78,55 @@ const ShoppingList = () => {
 	};
 
 	return (
-		<div className="flex flex-col">
-			<h2 className="hidden md:block font-bold">Shopping List</h2>
-			<div className="flex space-x-2 md:space-x-0 md:flex-wrap items-end justify-between pb-3">
+		<div className='flex flex-col'>
+			<h2 className='hidden md:block font-bold'>Shopping List</h2>
+			<div className='flex space-x-2 md:space-x-0 md:flex-wrap items-end justify-between pb-3'>
 				{/* Header and search */}
-				<div className="relative w-full md:mb-2">
+				<div className='relative w-full md:mb-2'>
 					<SearchBar
 						searchQuery={searchQuery}
 						setSearchQuery={setSearchQuery}
-						label="Search Extra Items"
+						label='Search Extra Items'
 					/>
 					{searchResults.length ? (
-						<div className="absolute top-full left-0 z-10 w-full bg-white border border-black-25 shadow">
+						<div className='absolute top-full left-0 z-10 w-full bg-white border border-black-25 shadow'>
 							{searchResults.map((result) => (
 								<Modal
 									key={result._id}
 									title={`Add extra ${result.name} to Shopping List`}
 									trigger={<p>{result.name}</p>}
 								>
-									<div className="flex flex-col items-center gap-4 p-8">
-										<div className="flex items-center justify-center gap-8 flex-grow mb-4">
-											<div className="flex flex-col gap-2">
-												<label htmlFor="">Amount</label>
+									<div className='flex flex-col items-center gap-4 p-8'>
+										<div className='flex items-center justify-center gap-8 flex-grow mb-4'>
+											<div className='flex flex-col gap-2'>
+												<label htmlFor=''>Amount</label>
 												<QuantityInput
 													value={extraQuantity}
 													setValue={setExtraQuantity}
 												/>
 											</div>
-											<div className="flex flex-col gap-2">
-												<label htmlFor="">Unit (if applicable)</label>
+											<div className='flex flex-col gap-2'>
+												<label htmlFor=''>Unit (if applicable)</label>
 												<select
-													className="border-2 rounded h-[44px] text-xl w-full py-1"
-													name=""
-													id=""
+													className='border-2 rounded h-[44px] text-xl w-full py-1'
+													name=''
+													id=''
 													onChange={(e) => setExtraUnit(e.target.value)}
 													value={extraUnit}
 												>
-													<option value="">-</option>
-													<option value="cans">Cans</option>
-													<option value="g">g</option>
-													<option value="ml">ml</option>
-													<option value="tbsp">Tablespoons</option>
-													<option value="tsp">Teaspoons</option>
+													<option value=''>-</option>
+													<option value='cans'>Cans</option>
+													<option value='g'>g</option>
+													<option value='ml'>ml</option>
+													<option value='tbsp'>Tablespoons</option>
+													<option value='tsp'>Teaspoons</option>
 												</select>
 											</div>
 										</div>
 										<Button
 											onClick={() => handleAddExtraItem(result)}
 											inverse
-											text="Add to Shopping List"
+											text='Add to Shopping List'
 										/>
 									</div>
 								</Modal>
@@ -131,23 +134,26 @@ const ShoppingList = () => {
 						</div>
 					) : null}
 				</div>
-				<div className="md:hidden">
+				<div className='md:hidden'>
 					<Button
 						onClick={() => clearExtraItems()}
-						type="primary"
+						type='primary'
 						border
-						text="Clear Extra Items"
+						text='Clear Extra Items'
 					/>
 				</div>
-				<div className="hidden md:flex justify-center md:space-y-2 lg:space-y-0 lg:space-x-2 w-full md:flex-col lg:flex-row">
-					<Button
-						onClick={() => clearExtraItems()}
-						type="primary"
-						border
-						text="Clear Extra Items"
-					/>
-					<Button type="primary" border text="Clear Menu" />
-				</div>
+
+				{recipeMenu.length ? (
+					<div className='hidden md:flex justify-center md:space-y-2 lg:space-y-0 lg:space-x-2 w-full md:flex-col lg:flex-row'>
+						<Button
+							onClick={() => clearExtraItems()}
+							type='primary'
+							border
+							text='Clear Extra Items'
+						/>
+						<Button type='primary' border text='Clear Menu' />
+					</div>
+				) : null}
 			</div>
 			{/* Shopping list */}
 			<Accordion items={accordionItems} />
--- client/src/app/your-crockpot/tabs/Menu/Menu.tsx
+++ client/src/app/your-crockpot/tabs/Menu/Menu.tsx
@@ -7,35 +7,37 @@ import useRecipes from '@/src/hooks/recipes/useRecipes';
 
 const Menu = () => {
 	const { allRecipes } = useRecipes();
-	const { recipeMenuRecipes } = useRecipeMenu();
+	const { recipeMenuRecipes, recipeMenu } = useRecipeMenu();
 
 	const suggestedRecipes = allRecipes.slice(0, 4);
 
 	return (
 		<>
-			<div className="container flex justify-center space-x-4 py-4 bg-white/90 sticky top-[74px] z-searchBar lg:hidden">
+			<div className='container flex justify-center space-x-4 py-4 bg-white/90 sticky top-[74px] z-searchBar lg:hidden'>
 				<Modal
 					title={`Shopping List`}
-					trigger={<Button type="primary" border text="Shopping List" />}
+					trigger={<Button type='primary' border text='Shopping List' />}
 				>
 					<ShoppingList />
 				</Modal>
-				<Button type="primary" border text="Clear Menu" />
+				{recipeMenu.length ? (
+					<Button type='primary' border text='Clear Menu' />
+				) : null}
 			</div>
-			<div className="container !px-0 lg:flex lg:pt-4">
-				<div className="container lg:w-1/3 lg:border-2 lg:border-black lg:rounded lg:pt-2 lg:mx-4 xl:ml-0 lg:my-0 lg:h-fit lg:max-h-[85vh] lg:overflow-scroll lg:sticky lg:top-[110px] xl:px-4">
-					<div className="hidden lg:block">
+			<div className='container !px-0 lg:flex lg:pt-4'>
+				<div className='container lg:w-1/3 lg:border-2 lg:border-black lg:rounded lg:pt-2 lg:mx-4 xl:ml-0 lg:my-0 lg:h-fit lg:max-h-[85vh] lg:overflow-scroll lg:sticky lg:top-[110px] xl:px-4'>
+					<div className='hidden lg:block'>
 						<ShoppingList />
 					</div>
 				</div>
-				<div className="lg:hidden">
+				<div className='lg:hidden'>
 					{recipeMenuRecipes.length !== 0 ? (
 						<RecipeCardList recipes={recipeMenuRecipes} fullWidth />
 					) : (
 						<>
-							<div className="text-center mb-4">
-								<h2 className="mb-2">Nothing on the menu yet?</h2>
-								<p className="h3 !leading-5">
+							<div className='text-center mb-4'>
+								<h2 className='mb-2'>Nothing on the menu yet?</h2>
+								<p className='h3 !leading-5'>
 									How about some of these? Click the shopping cart button to add
 									them to Your Crockpot.
 								</p>
@@ -44,14 +46,14 @@ const Menu = () => {
 						</>
 					)}
 				</div>
-				<div className="hidden container lg:block lg:w-2/3 lg:pl-4">
+				<div className='hidden container lg:block lg:w-2/3 lg:pl-4'>
 					{recipeMenuRecipes.length !== 0 ? (
 						<RecipeCardList recipes={recipeMenuRecipes} />
 					) : (
 						<>
-							<div className="text-center mb-4">
-								<h2 className="mb-2">Nothing on the menu yet?</h2>
-								<p className="h3 !leading-5">
+							<div className='text-center mb-4'>
+								<h2 className='mb-2'>Nothing on the menu yet?</h2>
+								<p className='h3 !leading-5'>
 									How about some of these? Click the shopping cart button to add
 									them to Your Crockpot.
 								</p>
