0007654fc65ba521a8ebb907e30c2ba944eea25c
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-01-14 15:26:50 +0000

    Fix ts issue

--- client/src/app/your-crockpot/tabs/MyRecipes/MyRecipes.tsx
+++ client/src/app/your-crockpot/tabs/MyRecipes/MyRecipes.tsx
@@ -1,23 +1,24 @@
 import RecipeCardList from '@/src/components/RecipeCardList/RecipeCardList';
 import useUser from '@/src/hooks/auth/useUser';
 import useRecipes from '@/src/hooks/recipes/useRecipes';
+import { Recipe } from '@/src/types/types';
 
 const MyRecipes = () => {
 	const { allRecipes } = useRecipes();
 	const { user } = useUser();
 
 	const myRecipes = user
-		? allRecipes.filter((recipe) => recipe.createdBy._id === user._id)
+		? allRecipes.filter((recipe: Recipe) => recipe.createdBy._id === user._id)
 		: null;
 
 	return (
-		<div className="pt-4 px-4">
+		<div className='pt-4 px-4'>
 			{myRecipes ? (
 				<RecipeCardList recipes={myRecipes} fullWidth />
 			) : (
-				<div className="text-center mb-4">
-					<h2 className="mb-2">No recipes yet?</h2>
-					<p className="h3 !leading-5">
+				<div className='text-center mb-4'>
+					<h2 className='mb-2'>No recipes yet?</h2>
+					<p className='h3 !leading-5'>
 						Click the plus button to add your own recipes to Your Crockpot.
 					</p>
 				</div>
