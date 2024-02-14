fd6604e677c8642aa0c233b5f47715408bac7234
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-02-14 09:01:04 +0000

    Implement new modal in recipes tab

--- src/app/admin/tabs/recipes/RecipesTab.tsx
+++ src/app/admin/tabs/recipes/RecipesTab.tsx
@@ -1,5 +1,7 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+
+import { Recipe } from '@/types/types';
 
 import { useDeleteRecipe } from '@/hooks/recipes/useAddEditRecipe';
 import useRecipes from '@/hooks/recipes/useRecipes';
@@ -7,13 +9,15 @@ import useRecipes from '@/hooks/recipes/useRecipes';
 import AddRecipe from '@/components/AddRecipe/AddRecipe';
 import Button from '@/components/Button/Button';
 import SearchBar from '@/components/FormSearchBar/SearchBar';
-import Modal from '@/components/Modal/Modal';
+import Modal2 from '@/components/Modal2/Modal2';
+import { useModal } from '@/components/Modal2/ModalContext';
+import OpenModal from '@/components/Modal2/OpenModal';
 
 import styles from '../../styles.module.scss';
 
 const RecipesTab = () => {
 	const [query, setQuery] = useState('');
-	const [addRecipeModalOpen, setAddRecipeModalOpen] = useState(false);
+	const [selectedRecipe, setSelectedRecipe] = useState<Recipe | null>();
 
 	const { allRecipes, filterRecipes } = useRecipes();
 
@@ -23,20 +27,20 @@ const RecipesTab = () => {
 		return filterRecipes(allRecipes, query);
 	}, [allRecipes, query, filterRecipes]);
 
+	const { openModals } = useModal();
+
+	useEffect(() => {
+		if (!openModals.length) {
+			setSelectedRecipe(null);
+		}
+	}, [openModals]);
+
 	return (
 		<div className={styles.adminTabs}>
-			<div className={styles.modalTrigger}>
-				<Modal
-					trigger={<Button type="primary" text="Add New Recipe"></Button>}
-					title="Add new recipe"
-					open={addRecipeModalOpen}
-					setOpen={setAddRecipeModalOpen}
-					paddingOn
-					size="md"
-				>
-					<AddRecipe setModal={setAddRecipeModalOpen} />
-				</Modal>
-			</div>
+			<OpenModal name="AddRecipe" styles="flex items-center my-4">
+				<Button type="primary" text="Add New Recipe"></Button>
+			</OpenModal>
+
 			<div className={styles.searchBar}>
 				<SearchBar
 					searchQuery={query}
@@ -51,50 +55,50 @@ const RecipesTab = () => {
 							<div className={styles.searchResult} key={result._id}>
 								<p>{result.name}</p>
 								<div className="flex space-x-2">
-									<Modal
-										trigger={
-											<Button type="primary">
-												<RiEdit2Line />
-											</Button>
-										}
-										title={
-											<>
-												Edit <i>{result.name}</i>
-											</>
-										}
-										paddingOn
-										size="md"
+									<OpenModal
+										onClick={() => setSelectedRecipe(result)}
+										name="EditRecipe"
+										// styles="flex space-x-2"
 									>
-										<AddRecipe recipe={result} />
-									</Modal>
-									<Modal
-										trigger={
-											<Button type="primary">
-												<RiDeleteBinLine />
-											</Button>
-										}
-										title={
-											<>
-												Delete <i>{result.name}</i>
-											</>
-										}
-										paddingOn
-										size="sm"
+										<Button type="primary">
+											<RiEdit2Line />
+										</Button>
+									</OpenModal>
+									<OpenModal
+										onClick={() => setSelectedRecipe(result)}
+										name="DeleteRecipe"
 									>
-										<div className="modal--p-and-button">
-											<p>Are you sure you&aposd like to delete this recipe?</p>
-											<Button
-												onClick={() => deleteRecipe(result._id)}
-												text="Delete Recipe"
-												border
-											/>
-										</div>
-									</Modal>
+										<Button type="primary">
+											<RiDeleteBinLine />
+										</Button>
+									</OpenModal>
 								</div>
 							</div>
 						))}
 					</div>
 				</>
+			) : null}
+			<Modal2 name={'AddRecipe'} title="Add new recipe">
+				<AddRecipe />
+			</Modal2>
+			{selectedRecipe && openModals.includes('EditRecipe') ? (
+				<Modal2 name="EditRecipe" title={`Edit ${selectedRecipe.name} `}>
+					<AddRecipe recipe={selectedRecipe} />
+				</Modal2>
+			) : null}
+			{selectedRecipe && openModals.includes('DeleteRecipe') ? (
+				<Modal2 name="DeleteRecipe" customSize="small">
+					<div className="modal--p-and-button">
+						<p className="text-center">
+							Are you sure you would like to delete this recipe?
+						</p>
+						<Button
+							onClick={() => deleteRecipe(selectedRecipe._id)}
+							text="Delete Recipe"
+							border
+						/>
+					</div>
+				</Modal2>
 			) : null}
 		</div>
 	);
