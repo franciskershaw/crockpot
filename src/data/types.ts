ebf0ceac2b78778a049a3d771577d20fb2bdb5bf
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2025-09-09 12:49:20 +0100

    Fix TS issues

--- src/data/types.ts
+++ src/data/types.ts
@@ -34,7 +34,6 @@ export interface Ingredient {
       id: string;
       name: string;
       faIcon: string;
-      defaultUnitIds: string[];
       createdAt: Date;
       updatedAt: Date;
     };
